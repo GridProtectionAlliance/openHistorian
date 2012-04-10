7b295207f0fcfa8ab336420e443672829294e8ff
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2012-04-10 19:31:22 +0000

    Added basic test infrastructure.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS11]$/openHistorian/Main;C15765

---
+++ Source/Libraries/Tests/openHistorian.V2.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace openHistorian.V2.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
