03a53e2b5a76466a626849eed15df5c4d27d32f0
StevenChisholm <steven@chizware.com>
2012-06-17 00:47:27 +0000

    Ran all current test modes.  Code back to functional state.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS11]$/openHistorian/Main;C17172

--- Source/Libraries/Tests/openHistorian.V2.Test/FileSystem/TransactionalEditTest.cs
+++ Source/Libraries/Tests/openHistorian.V2.Test/FileSystem/TransactionalEditTest.cs
@@ -1,157 +1,157 @@
-﻿////******************************************************************************************************
-////  TransactionalEditTest.cs - Gbtc
-////
-////  Copyright © 2012, Grid Protection Alliance.  All Rights Reserved.
-////
-////  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
-////  the NOTICE file distributed with this work for additional information regarding copyright ownership.
-////  The GPA licenses this file to you under the Eclipse Public License -v 1.0 (the "License"); you may
-////  not use this file except in compliance with the License. You may obtain a copy of the License at:
-////
-////      http://www.opensource.org/licenses/eclipse-1.0.php
-////
-////  Unless agreed to in writing, the subject software distributed under the License is distributed on an
-////  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
-////  License for the specific language governing permissions and limitations.
-////
-////  Code Modification History:
-////  ----------------------------------------------------------------------------------------------------
-////  12/2/2011 - Steven E. Chisholm
-////       Generated original version of source code.
-////
-////******************************************************************************************************
-//using System;
-//using System.Collections.Generic;
-//using System.Collections.ObjectModel;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿//******************************************************************************************************
+//  TransactionalEditTest.cs - Gbtc
+//
+//  Copyright © 2012, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the Eclipse Public License -v 1.0 (the "License"); you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://www.opensource.org/licenses/eclipse-1.0.php
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  12/2/2011 - Steven E. Chisholm
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-//namespace openHistorian.V2.FileSystem
-//{
-//    [TestClass()]
-//    public class TransactionalEditTest
-//    {
-//        [TestMethod()]
-//        public void Test()
-//        {
-//            Assert.AreEqual(openHistorian.V2.UnmanagedMemory.BufferPool.AllocatedBytes, 0L);
-//            DiskIoEnhanced stream = new DiskIoEnhanced();
-//            FileAllocationTable fat = FileAllocationTable.CreateFileAllocationTable(stream);
-//            //obtain a readonly copy of the file allocation table.
-//            fat = FileAllocationTable.OpenHeader(stream);
-//            TestCreateNewFile(stream, fat);
-//            fat = FileAllocationTable.OpenHeader(stream);
-//            TestOpenExistingFile(stream, fat);
-//            fat = FileAllocationTable.OpenHeader(stream);
-//            TestRollback(stream, fat);
-//            fat = FileAllocationTable.OpenHeader(stream);
-//            TestVerifyRollback(stream, fat);
-//            Assert.IsTrue(true);
-//            stream.Dispose();
-//            Assert.AreEqual(openHistorian.V2.UnmanagedMemory.BufferPool.AllocatedBytes, 0L);
-//        }
-//        static void TestCreateNewFile(DiskIoEnhanced stream, FileAllocationTable fat)
-//        {
-//            Guid id = Guid.NewGuid();
-//            TransactionalEdit trans = new TransactionalEdit(stream, fat);
-//            //create 3 files
+namespace openHistorian.V2.FileSystem
+{
+    [TestClass()]
+    public class TransactionalEditTest
+    {
+        [TestMethod()]
+        public void Test()
+        {
+            Assert.AreEqual(Globals.BufferPool.AllocatedBytes, 0L);
+            DiskIoEnhanced stream = new DiskIoEnhanced();
+            FileAllocationTable fat = FileAllocationTable.CreateFileAllocationTable(stream);
+            //obtain a readonly copy of the file allocation table.
+            fat = FileAllocationTable.OpenHeader(stream);
+            TestCreateNewFile(stream, fat);
+            fat = FileAllocationTable.OpenHeader(stream);
+            TestOpenExistingFile(stream, fat);
+            fat = FileAllocationTable.OpenHeader(stream);
+            TestRollback(stream, fat);
+            fat = FileAllocationTable.OpenHeader(stream);
+            TestVerifyRollback(stream, fat);
+            Assert.IsTrue(true);
+            stream.Dispose();
+            Assert.AreEqual(Globals.BufferPool.AllocatedBytes, 0L);
+        }
+        static void TestCreateNewFile(DiskIoEnhanced stream, FileAllocationTable fat)
+        {
+            Guid id = Guid.NewGuid();
+            TransactionalEdit trans = new TransactionalEdit(stream, fat);
+            //create 3 files
 
-//            ArchiveFileStream fs1 = trans.CreateFile(id, 1234);
-//            ArchiveFileStream fs2 = trans.CreateFile(id, 1234);
-//            ArchiveFileStream fs3 = trans.CreateFile(id, 1234);
-//            if (fs1.File.FileExtension != id)
-//                throw new Exception();
-//            if (fs1.File.FileFlags != 1234)
-//                throw new Exception();
-//            //write to the three files
-//            ArchiveFileStreamTest.TestSingleByteWrite(fs1);
-//            ArchiveFileStreamTest.TestCustomSizeWrite(fs2, 5);
-//            ArchiveFileStreamTest.TestCustomSizeWrite(fs3, ArchiveConstants.DataBlockDataLength + 20);
+            ArchiveFileStream fs1 = trans.CreateFile(id, 1234);
+            ArchiveFileStream fs2 = trans.CreateFile(id, 1234);
+            ArchiveFileStream fs3 = trans.CreateFile(id, 1234);
+            if (fs1.File.FileExtension != id)
+                throw new Exception();
+            if (fs1.File.FileFlags != 1234)
+                throw new Exception();
+            //write to the three files
+            ArchiveFileStreamTest.TestSingleByteWrite(fs1);
+            ArchiveFileStreamTest.TestCustomSizeWrite(fs2, 5);
+            ArchiveFileStreamTest.TestCustomSizeWrite(fs3, ArchiveConstants.DataBlockDataLength + 20);
 
-//            //read from them and verify content.
-//            ArchiveFileStreamTest.TestCustomSizeRead(fs3, ArchiveConstants.DataBlockDataLength + 20);
-//            ArchiveFileStreamTest.TestCustomSizeRead(fs2, 5);
-//            ArchiveFileStreamTest.TestSingleByteRead(fs1);
+            //read from them and verify content.
+            ArchiveFileStreamTest.TestCustomSizeRead(fs3, ArchiveConstants.DataBlockDataLength + 20);
+            ArchiveFileStreamTest.TestCustomSizeRead(fs2, 5);
+            ArchiveFileStreamTest.TestSingleByteRead(fs1);
 
-//            trans.Commit();
-//        }
+            trans.Commit();
+        }
 
-//        static void TestOpenExistingFile(DiskIoEnhanced stream, FileAllocationTable fat)
-//        {
-//            Guid id = Guid.NewGuid();
-//            TransactionalEdit trans = new TransactionalEdit(stream, fat);
-//            //create 3 files
+        static void TestOpenExistingFile(DiskIoEnhanced stream, FileAllocationTable fat)
+        {
+            Guid id = Guid.NewGuid();
+            TransactionalEdit trans = new TransactionalEdit(stream, fat);
+            //create 3 files
 
-//            ArchiveFileStream fs1 = trans.OpenFile(0);
-//            ArchiveFileStream fs2 = trans.OpenFile(1);
-//            ArchiveFileStream fs3 = trans.OpenFile(2);
+            ArchiveFileStream fs1 = trans.OpenFile(0);
+            ArchiveFileStream fs2 = trans.OpenFile(1);
+            ArchiveFileStream fs3 = trans.OpenFile(2);
 
-//            //read from them and verify content.
-//            ArchiveFileStreamTest.TestSingleByteRead(fs1);
-//            ArchiveFileStreamTest.TestCustomSizeRead(fs2, 5);
-//            ArchiveFileStreamTest.TestCustomSizeRead(fs3, ArchiveConstants.DataBlockDataLength + 20);
+            //read from them and verify content.
+            ArchiveFileStreamTest.TestSingleByteRead(fs1);
+            ArchiveFileStreamTest.TestCustomSizeRead(fs2, 5);
+            ArchiveFileStreamTest.TestCustomSizeRead(fs3, ArchiveConstants.DataBlockDataLength + 20);
 
-//            //rewrite bad data.
-//            ArchiveFileStreamTest.TestSingleByteWrite(fs2);
-//            ArchiveFileStreamTest.TestCustomSizeWrite(fs3, 5);
-//            ArchiveFileStreamTest.TestCustomSizeWrite(fs1, ArchiveConstants.DataBlockDataLength + 20);
+            //rewrite bad data.
+            ArchiveFileStreamTest.TestSingleByteWrite(fs2);
+            ArchiveFileStreamTest.TestCustomSizeWrite(fs3, 5);
+            ArchiveFileStreamTest.TestCustomSizeWrite(fs1, ArchiveConstants.DataBlockDataLength + 20);
 
-//            //verify origional still in tact.
-//            ArchiveFileStream fs11 = trans.OpenOrigionalFile(0);
-//            ArchiveFileStream fs12 = trans.OpenOrigionalFile(1);
-//            ArchiveFileStream fs13 = trans.OpenOrigionalFile(2);
+            //verify origional still in tact.
+            ArchiveFileStream fs11 = trans.OpenOrigionalFile(0);
+            ArchiveFileStream fs12 = trans.OpenOrigionalFile(1);
+            ArchiveFileStream fs13 = trans.OpenOrigionalFile(2);
 
-//            ArchiveFileStreamTest.TestSingleByteRead(fs11);
-//            ArchiveFileStreamTest.TestCustomSizeRead(fs12, 5);
-//            ArchiveFileStreamTest.TestCustomSizeRead(fs13, ArchiveConstants.DataBlockDataLength + 20);
-//            trans.Commit();
-//        }
+            ArchiveFileStreamTest.TestSingleByteRead(fs11);
+            ArchiveFileStreamTest.TestCustomSizeRead(fs12, 5);
+            ArchiveFileStreamTest.TestCustomSizeRead(fs13, ArchiveConstants.DataBlockDataLength + 20);
+            trans.Commit();
+        }
 
-//        static void TestRollback(DiskIoEnhanced stream, FileAllocationTable fat)
-//        {
-//            Guid id = Guid.NewGuid();
-//            TransactionalEdit trans = new TransactionalEdit(stream, fat);
+        static void TestRollback(DiskIoEnhanced stream, FileAllocationTable fat)
+        {
+            Guid id = Guid.NewGuid();
+            TransactionalEdit trans = new TransactionalEdit(stream, fat);
 
-//            //create 3 files additional files
-//            ArchiveFileStream fs21 = trans.CreateFile(id, 1234);
-//            ArchiveFileStream fs22 = trans.CreateFile(id, 1234);
-//            ArchiveFileStream fs23 = trans.CreateFile(id, 1234);
+            //create 3 files additional files
+            ArchiveFileStream fs21 = trans.CreateFile(id, 1234);
+            ArchiveFileStream fs22 = trans.CreateFile(id, 1234);
+            ArchiveFileStream fs23 = trans.CreateFile(id, 1234);
 
-//            //open files
-//            ArchiveFileStream fs1 = trans.OpenFile(0);
-//            ArchiveFileStream fs2 = trans.OpenFile(1);
-//            ArchiveFileStream fs3 = trans.OpenFile(2);
+            //open files
+            ArchiveFileStream fs1 = trans.OpenFile(0);
+            ArchiveFileStream fs2 = trans.OpenFile(1);
+            ArchiveFileStream fs3 = trans.OpenFile(2);
 
-//            //read from them and verify content.
-//            ArchiveFileStreamTest.TestSingleByteRead(fs2);
-//            ArchiveFileStreamTest.TestCustomSizeRead(fs3, 5);
-//            ArchiveFileStreamTest.TestCustomSizeRead(fs1, ArchiveConstants.DataBlockDataLength + 20);
+            //read from them and verify content.
+            ArchiveFileStreamTest.TestSingleByteRead(fs2);
+            ArchiveFileStreamTest.TestCustomSizeRead(fs3, 5);
+            ArchiveFileStreamTest.TestCustomSizeRead(fs1, ArchiveConstants.DataBlockDataLength + 20);
 
-//            //rewrite bad data.
-//            ArchiveFileStreamTest.TestSingleByteWrite(fs3);
-//            ArchiveFileStreamTest.TestCustomSizeWrite(fs1, 5);
-//            ArchiveFileStreamTest.TestCustomSizeWrite(fs2, ArchiveConstants.DataBlockDataLength + 20);
+            //rewrite bad data.
+            ArchiveFileStreamTest.TestSingleByteWrite(fs3);
+            ArchiveFileStreamTest.TestCustomSizeWrite(fs1, 5);
+            ArchiveFileStreamTest.TestCustomSizeWrite(fs2, ArchiveConstants.DataBlockDataLength + 20);
 
-//            trans.Rollback();
-//        }
+            trans.Rollback();
+        }
 
-//        static void TestVerifyRollback(DiskIoEnhanced stream, FileAllocationTable fat)
-//        {
-//            Guid id = Guid.NewGuid();
-//            TransactionalEdit trans = new TransactionalEdit(stream, fat);
+        static void TestVerifyRollback(DiskIoEnhanced stream, FileAllocationTable fat)
+        {
+            Guid id = Guid.NewGuid();
+            TransactionalEdit trans = new TransactionalEdit(stream, fat);
 
-//            if (trans.Files.Count != 3)
-//                throw new Exception();
+            if (trans.Files.Count != 3)
+                throw new Exception();
 
-//            //open files
-//            ArchiveFileStream fs1 = trans.OpenFile(0);
-//            ArchiveFileStream fs2 = trans.OpenFile(1);
-//            ArchiveFileStream fs3 = trans.OpenFile(2);
+            //open files
+            ArchiveFileStream fs1 = trans.OpenFile(0);
+            ArchiveFileStream fs2 = trans.OpenFile(1);
+            ArchiveFileStream fs3 = trans.OpenFile(2);
 
-//            //read from them and verify content.
-//            ArchiveFileStreamTest.TestSingleByteRead(fs2);
-//            ArchiveFileStreamTest.TestCustomSizeRead(fs3, 5);
-//            ArchiveFileStreamTest.TestCustomSizeRead(fs1, ArchiveConstants.DataBlockDataLength + 20);
-//            trans.Dispose();
-//        }
-//    }
-//}
+            //read from them and verify content.
+            ArchiveFileStreamTest.TestSingleByteRead(fs2);
+            ArchiveFileStreamTest.TestCustomSizeRead(fs3, 5);
+            ArchiveFileStreamTest.TestCustomSizeRead(fs1, ArchiveConstants.DataBlockDataLength + 20);
+            trans.Dispose();
+        }
+    }
+}
