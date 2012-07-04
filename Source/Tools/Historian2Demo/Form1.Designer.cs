8609eea7ce811d0a8e4f20b4df85488fa7363cc3
StevenChisholm <steven@chizware.com>
2012-07-04 19:34:20 +0000

    Added Historian2Demo project which will be used for extensive testing of the historian.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS11]$/openHistorian/Main;C17682

---
+++ Source/Tools/Historian2Demo/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Historian2Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnStartEngine = new System.Windows.Forms.Button();
+            this.BtnAttachFile = new System.Windows.Forms.Button();
+            this.BtnCreatePoints = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnStartEngine
+            // 
+            this.BtnStartEngine.Location = new System.Drawing.Point(12, 12);
+            this.BtnStartEngine.Name = "BtnStartEngine";
+            this.BtnStartEngine.Size = new System.Drawing.Size(92, 23);
+            this.BtnStartEngine.TabIndex = 0;
+            this.BtnStartEngine.Text = "Start Engine";
+            this.BtnStartEngine.UseVisualStyleBackColor = true;
+            this.BtnStartEngine.Click += new System.EventHandler(this.BtnStartEngine_Click);
+            // 
+            // BtnAttachFile
+            // 
+            this.BtnAttachFile.Location = new System.Drawing.Point(12, 41);
+            this.BtnAttachFile.Name = "BtnAttachFile";
+            this.BtnAttachFile.Size = new System.Drawing.Size(121, 23);
+            this.BtnAttachFile.TabIndex = 1;
+            this.BtnAttachFile.Text = "Attach WAV File";
+            this.BtnAttachFile.UseVisualStyleBackColor = true;
+            // 
+            // BtnCreatePoints
+            // 
+            this.BtnCreatePoints.Location = new System.Drawing.Point(264, 12);
+            this.BtnCreatePoints.Name = "BtnCreatePoints";
+            this.BtnCreatePoints.Size = new System.Drawing.Size(104, 23);
+            this.BtnCreatePoints.TabIndex = 2;
+            this.BtnCreatePoints.Text = "Create Points";
+            this.BtnCreatePoints.UseVisualStyleBackColor = true;
+            this.BtnCreatePoints.Click += new System.EventHandler(this.BtnCreatePoints_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 426);
+            this.Controls.Add(this.BtnCreatePoints);
+            this.Controls.Add(this.BtnAttachFile);
+            this.Controls.Add(this.BtnStartEngine);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnStartEngine;
+        private System.Windows.Forms.Button BtnAttachFile;
+        private System.Windows.Forms.Button BtnCreatePoints;
+    }
+}
+
---
+++ Source/Tools/Historian2Demo/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using openHistorian.V2.Server;
+using openHistorian.V2.Server.Database;
+using TVA.Media;
+
+namespace Historian2Demo
+{
+    public partial class Form1 : Form
+    {
+        public ServerInstance m_server;
+        public DatabaseEngine m_database;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void BtnAttachFile_Click(object sender, EventArgs e)
+        //{
+        //    OpenFileDialog dlg = new OpenFileDialog();
+        //    dlg.Filter = "Wav File|*.wav";
+        //    if (dlg.ShowDialog() == DialogResult.OK)
+        //    {
+        //        ProcessAudioFile(dlg.FileName);
+        //    }
+        //}
+
+        private void BtnStartEngine_Click(object sender, EventArgs e)
+        {
+            m_server = new ServerInstance();
+            m_server.Create("Audio");
+            m_database = m_server.Get("Audio");
+        }
+
+        private void BtnCreatePoints_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < 100000; x++)
+            {
+                Thread.Sleep(1);
+                DateTime currentTime = DateTime.Now;
+                m_database.WriteData((ulong)currentTime.Ticks, 1, 2, 3);
+            }
+        }
+
+        //private void ProcessAudioFile(string fileName)
+        //{
+        //    //var wave = WaveFile.Load(fileName, false);
+        //    var reader = WaveDataReader.FromFile(fileName);
+
+        //    while (true)
+        //    {
+        //        var sample = reader.GetNextSample();
+        //        if (sample != null)
+        //        {
+        //            double value = sample[0].ConvertToType(TypeCode.Double);
+        //        }
+        //        else
+        //        {
+        //            break;
+        //        }
+        //    }
+        //}
+
+
+    }
+}
