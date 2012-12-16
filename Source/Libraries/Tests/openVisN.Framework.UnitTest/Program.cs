1d0befdecfa0bdddadaff6dc77654bdd69ea5d60
StevenChisholm <steven@chizware.com>
2012-12-16 14:07:37 +0000

    Renamed openVisN to openVisN.Framework. Created openVisN.Winforms and openVisN.WPF projects. Moved SimpleVisN and FrameworkVisN into Tools folder. Created openVisN.Framework.UnitTests project.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS11]$/openHistorian/Main;C21993

---
+++ Source/Libraries/Tests/openVisN.Framework.UnitTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace openVisN.Framework.UnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
