2f7cdb4718910604fc42e4fea37153a83ffd48a5
StephenCWills <swills@gridprotectionalliance.org>
2013-08-19 14:45:34 +0000

    openHistorian: Version change for build openHistorian_v2.0.10.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS11]$/openHistorian/Main;C30194

--- Source/Applications/openHistorian/openHistorianConsole/Properties/AssemblyInfo.cs
+++ Source/Applications/openHistorian/openHistorianConsole/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
 
-[assembly: AssemblyVersion("2.0.9.0")]
+[assembly: AssemblyVersion("2.0.10.0")]
 
 // Informational attributes.
 
--- Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.0.9.0")]
+// [assembly: AssemblyVersion("2.0.10.0")]
 
-[assembly: AssemblyVersion("2.0.9.0")]
+[assembly: AssemblyVersion("2.0.10.0")]
