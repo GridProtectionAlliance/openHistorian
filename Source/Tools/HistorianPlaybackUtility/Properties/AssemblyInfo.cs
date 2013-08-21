372913b857fc98a7ce3ddbe3aef62ca389c43ddb
StephenCWills <swills@gridprotectionalliance.org>
2013-08-21 19:30:09 +0000

    openHistorian: Version change for build openHistorian_v2.0.12.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS11]$/openHistorian/Main;C30280

--- Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/HistorianPlaybackUtility/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.0.11.0")]
+[assembly: AssemblyVersion("2.0.12.0")]
 
 // Informational attributes.
 [assembly: AssemblyTitle("HistorianPlaybackUtility")]
