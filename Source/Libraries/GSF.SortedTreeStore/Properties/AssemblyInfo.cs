d4a90efe53f92248cb451e7553ed2f0bc8360f3d
StevenChisholm <steven@chizware.com>
2013-11-09 03:58:33 +0000

    Renamed project folders to fit project names.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS11]$/openHistorian/Main;C31956

--- Source/Libraries/GSF.SortedTreeStore/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.SortedTreeStore/Properties/AssemblyInfo.cs
@@ -5,22 +5,25 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("openHistorian.TimeSeries")]
+
+[assembly: AssemblyTitle("FileArchitecture")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("openHistorian.TimeSeries")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("FileArchitecture")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7528b6f9-979f-4de2-91a0-8ffa11f011c1")]
+
+[assembly: Guid("5c10aa8a-4b48-4c2d-8aed-68cca88da36a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +35,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("2.0.18.0")]
+
 [assembly: AssemblyVersion("2.0.18.0")]
 [assembly: AssemblyFileVersion("2.0.18.0")]
+[assembly: InternalsVisibleTo("GSF.SortedTreeStore.Test")]
--- Source/Libraries/openHistorian.Core/Properties/AssemblyInfo.cs
+++ Source/Libraries/openHistorian.Core/Properties/AssemblyInfo.cs
@@ -5,25 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("FileArchitecture")]
+[assembly: AssemblyTitle("openHistorian.TimeSeries")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("FileArchitecture")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("openHistorian.TimeSeries")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("5c10aa8a-4b48-4c2d-8aed-68cca88da36a")]
+[assembly: Guid("7528b6f9-979f-4de2-91a0-8ffa11f011c1")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,7 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("2.0.18.0")]
-
 [assembly: AssemblyVersion("2.0.18.0")]
 [assembly: AssemblyFileVersion("2.0.18.0")]
-[assembly: InternalsVisibleTo("GSF.SortedTreeStore.Test")]
