0059ae4c122bda8067a25c18714e5c70c91cdbf6
StevenChisholm <steven@chizware.com>
2013-11-09 03:35:59 +0000

    Added missing souce code files that did not make it in change set 31954
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS11]$/openHistorian/Main;C31955

---
+++ Source/Libraries/openHistorian.Core/IO/Unmanaged/CollectionEventArgs.cs
@@ -0,0 +1,90 @@
+﻿//******************************************************************************************************
+//  CollectionEventArgs.cs - Gbtc
+//
+//  Copyright © 2013, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the Eclipse Public License -v 1.0 (the "License"); you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://www.opensource.org/licenses/eclipse-1.0.php
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  8/18/2012 - Steven E. Chisholm
+//       Generated original version of source code. 
+//       
+//
+//******************************************************************************************************
+
+using System;
+
+namespace GSF.IO.Unmanaged
+{
+    public enum BufferPoolCollectionMode
+    {
+        None,
+        Normal,
+        Emergency,
+        Critical
+    }
+
+    /// <summary>
+    /// This contains information about the collection that is requested from the system.
+    /// </summary>
+    public class CollectionEventArgs : EventArgs
+    {
+        private readonly Func<int, bool> m_releasePage;
+
+        /// <summary>
+        /// When <see cref="CollectionMode"/> is <see cref="BufferPoolCollectionMode.Emergency"/> or 
+        /// <see cref="BufferPoolCollectionMode.Critical"/> this field contains the number of pages
+        /// that need to be released by all of the objects. This value will automatically decrement
+        /// every time a page has been released.
+        /// </summary>
+        public int DesiredPageReleaseCount
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// The mode for the collection
+        /// </summary>
+        public BufferPoolCollectionMode CollectionMode
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="CollectionEventArgs"/>.
+        /// </summary>
+        /// <param name="releasePage"></param>
+        /// <param name="collectionMode"></param>
+        /// <param name="desiredPageReleaseCount"></param>
+        public CollectionEventArgs(Func<int, bool> releasePage, BufferPoolCollectionMode collectionMode, int desiredPageReleaseCount)
+        {
+            DesiredPageReleaseCount = desiredPageReleaseCount;
+            m_releasePage = releasePage;
+            CollectionMode = collectionMode;
+        }
+
+        /// <summary>
+        /// Releases an unused page.
+        /// </summary>
+        /// <param name="index">the index of the page</param>
+        public void ReleasePage(int index)
+        {
+            if (m_releasePage(index))
+            {
+                DesiredPageReleaseCount--;
+            }
+        }
+    }
+}
---
+++ Source/Libraries/openHistorian.Core/IO/Unmanaged/Memory.cs
@@ -0,0 +1,260 @@
+﻿//******************************************************************************************************
+//  Memory.cs - Gbtc
+//
+//  Copyright © 2013, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the Eclipse Public License -v 1.0 (the "License"); you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://www.opensource.org/licenses/eclipse-1.0.php
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  3/18/2012 - Steven E. Chisholm
+//       Generated original version of source code. 
+//  6/8/2012 - Steven E. Chisholm
+//       Removed large page support and simplified unused and untested procedures for initial release     
+//
+//******************************************************************************************************
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace GSF.IO.Unmanaged
+{
+    /// <summary>
+    /// This class is used to allocate and free unmanaged memory.  To release memory allocated throught this class,
+    /// call the Dispose method of the return value.
+    /// </summary>
+    /// <remarks>
+    /// .NET does not respond well when managing tens of GBs of ram.  If a very large buffer pool must be created,
+    /// it would be good to allocate that buffer pool in unmanaged memory.
+    /// </remarks>
+    // ToDo: Consider adding support for managed memory allocation. By allocating a Byte[] and using GCHandle.Alloc() to pin the object.
+    // ToDo: Support Large block allocations via the OS.
+    public sealed class Memory 
+        : IDisposable
+    {
+        #region [ Members ]
+
+        /// <summary>
+        /// The pointer to the first byte of this unmanaged memory.
+        /// </summary>
+        public IntPtr Address
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// The number of bytes in this allocation.
+        /// </summary>
+        public int Size
+        {
+            get;
+            private set;
+        }
+
+        private bool m_disposed;
+        //bool m_isLargePage;
+
+        #endregion
+
+        #region [ Constructors ]
+
+        /// <summary>
+        /// Allocates unmanaged memory. The block is uninitialized.
+        /// </summary>
+        /// <param name="requestedSize">The desired number of bytes to allocate. 
+        /// Be sure to check the actual size in the return class. </param>
+        /// <returns>The allocated memory.</returns>
+        public Memory(int requestedSize)
+        {
+            if (requestedSize <= 0)
+                throw new ArgumentOutOfRangeException("requestedSize", "must be greater than zero");
+            //if (UseLargePages && (requestedSize & (s_largePageMinimumSize - 1)) == 0)
+            //{
+            //    Address = WinApi.LargePageSupport.VirtualAllocLargePages((uint)requestedSize);
+            //    if (Address != IntPtr.Zero)
+            //    {
+            //        Size = requestedSize;
+            //        m_isLargePage = true;
+            //        return;
+            //    }
+            //}
+
+            Address = Marshal.AllocHGlobal(requestedSize);
+            Size = requestedSize;
+        }
+
+        /// <summary>
+        /// Releases the unmanaged resources before the <see cref="Memory"/> object is reclaimed by <see cref="GC"/>.
+        /// </summary>
+        ~Memory()
+        {
+            Dispose(false);
+        }
+
+        #endregion
+
+        #region [ Methods ]
+
+        /// <summary>
+        /// Releases the allocated memory back to the OS.
+        /// Same thing as calling Dispose().
+        /// </summary>
+        public void Release()
+        {
+            Dispose();
+        }
+
+        /// <summary>
+        /// Releases all the resources used by the <see cref="Memory"/> object.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Releases the unmanaged resources used by the <see cref="Memory"/> object and optionally releases the managed resources.
+        /// </summary>
+        /// <param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
+        private void Dispose(bool disposing)
+        {
+            if (!m_disposed)
+            {
+                try
+                {
+                    // This will be done regardless of whether the object is finalized or disposed.
+                    if (Address != IntPtr.Zero)
+                    {
+                        //if (m_isLargePage)
+                        //    WinApi.LargePageSupport.VirtualFree(Address);
+                        //else
+                        Marshal.FreeHGlobal(Address);
+                    }
+                }
+                finally
+                {
+                    Size = 0;
+                    Address = IntPtr.Zero;
+                    m_disposed = true; // Prevent duplicate dispose.
+                }
+            }
+        }
+
+        #endregion
+
+        #region [ Static ]
+
+        #region [ Fields ]
+
+        //static bool s_useLargePageSizes;
+        //static uint s_largePageMinimumSize;
+        //public static bool UseLargePages
+        //{
+        //    get
+        //    {
+        //        return s_useLargePageSizes;
+        //    }
+        //    set
+        //    {
+        //        s_useLargePageSizes = value && WinApi.LargePageSupport.CanAllocateLargePage;
+        //    }
+        //}
+
+        #endregion
+
+        #region [ Constructor ]
+
+        //static Memory()
+        //{
+        //    //if (WinApi.LargePageSupport.CanAllocateLargePage)
+        //    //    s_largePageMinimumSize = WinApi.LargePageSupport.GetLargePageMinimum();
+        //    //else
+        //        s_largePageMinimumSize = 0;
+        //}
+
+        #endregion
+
+        #region [ Methods ]
+
+        /// <summary>
+        /// Does a safe copy of data from one location to another. 
+        /// A safe copy allows for the source and destination to overlap.
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="dest"></param>
+        /// <param name="count"></param>
+        public static unsafe void Copy(byte* src, byte* dest, int count)
+        {
+            WinApi.MoveMemory(dest, src, count);
+        }
+
+        public static unsafe void Copy(IntPtr src, IntPtr dest, int count)
+        {
+            WinApi.MoveMemory((byte*)dest, (byte*)src, count);
+        }
+
+        /// <summary>
+        /// Sets the data in this buffer to all zeroes
+        /// </summary>
+        /// <param name="pointer">the starting position.</param>
+        /// <param name="length">the number of bytes to clear.</param>
+        public static unsafe void Clear(byte* pointer, int length)
+        {
+            int i;
+            for (i = 0; i <= length - 8; i += 8)
+            {
+                *(long*)(pointer + i) = 0;
+            }
+            for (; i < length; i++)
+            {
+                pointer[i] = 0;
+            }
+        }
+
+        /// <summary>
+        /// Sets the data in this buffer to all zeroes
+        /// </summary>
+        /// <param name="pointer">the starting position.</param>
+        /// <param name="length">the number of bytes to clear.</param>
+        public static unsafe void Clear(IntPtr pointer, int length)
+        {
+            Clear((byte*)pointer, length);
+        }
+
+        public static unsafe bool IsEmpty(IntPtr pointer, int length)
+        {
+            return IsEmpty((byte*)pointer, length);
+        }
+
+        public static unsafe bool IsEmpty(byte* pointer, int length)
+        {
+            int i;
+            for (i = 0; i < length - 8; i += 8)
+            {
+                if (*(long*)(pointer + i) != 0)
+                    return false;
+            }
+            for (; i < length; i++)
+            {
+                if (pointer[i] != 0)
+                    return false;
+            }
+            return true;
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
