ed1c1b0281b29c9f834828170b20c019e4620631
gsfbuildbot <support@gridprotectionalliance.org>
2015-02-14 08:02:47 +0000

    openHistorian: Version change for build openHistorian_v2.0.126.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS11]$/openHistorian/Main;C41950

--- Source/Applications/openHistorian/openHistorianConsole/Properties/AssemblyInfo.cs
+++ Source/Applications/openHistorian/openHistorianConsole/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
 
-[assembly: AssemblyVersion("2.0.125.0")]
+[assembly: AssemblyVersion("2.0.126.0")]
 
 // Informational attributes.
 
