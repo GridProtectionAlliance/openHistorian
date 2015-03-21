d59c7f7d2aaffc9160e36b02304fadf017ef62d4
gsfbuildbot <support@gridprotectionalliance.org>
2015-03-21 06:58:24 +0000

    Updated database schema definitions from GSF source.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS11]$/openHistorian/Main;C42233

--- Source/Data/SQLite/db-update.bat
+++ Source/Data/SQLite/db-update.bat
@@ -5,10 +5,10 @@
 ::
 ::  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
 ::  the NOTICE file distributed with this work for additional information regarding copyright ownership.
-::  The GPA licenses this file to you under the Eclipse Public License -v 1.0 (the "License"); you may
+::  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may
 ::  not use this file except in compliance with the License. You may obtain a copy of the License at:
 ::
-::      http://www.opensource.org/licenses/eclipse-1.0.php
+::      http://www.opensource.org/licenses/MIT
 ::
 ::  Unless agreed to in writing, the subject software distributed under the License is distributed on an
 ::  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
