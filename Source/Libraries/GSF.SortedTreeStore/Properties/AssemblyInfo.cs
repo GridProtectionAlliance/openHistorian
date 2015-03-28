64625e8d2f0b445c0042cabb94e93fcf4c2ec6f3
gsfbuildbot <support@gridprotectionalliance.org>
2015-03-28 04:29:46 +0000

    openHistorian: Version change for build openHistorian_v2.0.145.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS11]$/openHistorian/Main;C42294

--- Source/Libraries/GSF.SortedTreeStore/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.SortedTreeStore/Properties/AssemblyInfo.cs
@@ -34,9 +34,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.0.144.0")]
+// [assembly: AssemblyVersion("2.0.145.0")]
 
-[assembly: AssemblyVersion("2.0.144.0")]
-[assembly: AssemblyFileVersion("2.0.144.0")]
+[assembly: AssemblyVersion("2.0.145.0")]
+[assembly: AssemblyFileVersion("2.0.145.0")]
 [assembly: InternalsVisibleTo("GSF.SortedTreeStore.Test")]
 [assembly: InternalsVisibleTo("openHistorian.PerformanceTests")]
