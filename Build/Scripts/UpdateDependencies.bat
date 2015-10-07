a39c00556378fd8d0dc06fb2bd7cbede332a63f0
StephenCWills <swills@gridprotectionalliance.org>
2015-10-07 09:50:44 -0400

    Modified the update dependencies script to reset the local repository to the head of the upstream branch when updating the local repository.

--- Build/Scripts/UpdateDependencies.bat
+++ Build/Scripts/UpdateDependencies.bat
@@ -57,9 +57,9 @@ ECHO Getting latest version...
 :UpdateRepository
 ECHO.
 ECHO Updating to latest version...
-%git% reset --hard
+%git% fetch
+%git% reset --hard origin/master
 %git% clean -f -d -x
-%git% pull
 
 ECHO.
 ECHO Updating dependencies...
