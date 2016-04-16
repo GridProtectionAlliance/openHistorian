5ab8802676c5c3b1e2dd06c391d7a2f3290b8d34
gsfbuildbot <support@gridprotectionalliance.org>
2016-04-16 00:43:04 -0400

    openHistorian: Version change for build v2.0.300.0-master.

--- Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.0.299.0")]
+// [assembly: AssemblyVersion("2.0.300.0")]
 
-[assembly: AssemblyVersion("2.0.299.0")]
+[assembly: AssemblyVersion("2.0.300.0")]
