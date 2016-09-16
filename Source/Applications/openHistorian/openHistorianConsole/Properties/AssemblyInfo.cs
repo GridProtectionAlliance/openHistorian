e1756ad34131de46f3eb5de98e352910fd967d19
gsfbuildbot <support@gridprotectionalliance.org>
2016-09-16 00:39:35 -0400

    openHistorian: Version change for build v2.0.400.0-master.

--- Source/Applications/openHistorian/openHistorianConsole/Properties/AssemblyInfo.cs
+++ Source/Applications/openHistorian/openHistorianConsole/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
 
-[assembly: AssemblyVersion("2.0.399.0")]
+[assembly: AssemblyVersion("2.0.400.0")]
 
 // Informational attributes.
 
--- Source/Libraries/GSF.SortedTreeStore/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.SortedTreeStore/Properties/AssemblyInfo.cs
@@ -34,9 +34,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.0.399.0")]
+// [assembly: AssemblyVersion("2.0.400.0")]
 
-[assembly: AssemblyVersion("2.0.399.0")]
-[assembly: AssemblyFileVersion("2.0.399.0")]
+[assembly: AssemblyVersion("2.0.400.0")]
+[assembly: AssemblyFileVersion("2.0.400.0")]
 [assembly: InternalsVisibleTo("GSF.SortedTreeStore.Test")]
 [assembly: InternalsVisibleTo("openHistorian.PerformanceTests")]
