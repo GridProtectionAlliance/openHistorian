592b5ba817f49a00961cb9c71241885f7537fdff
StephenCWills <swills@gridprotectionalliance.org>
2016-10-10 10:53:01 -0400

    Updated ConfigurationUtility and MigrationUtility to ensure that both apps run as admin and code signing is enabled.

--- Source/Tools/ComparisonUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/ComparisonUtility/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MigrationUtility")]
+[assembly: AssemblyTitle("ComparisonUtility")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MigrationUtility")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("ComparisonUtility")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
