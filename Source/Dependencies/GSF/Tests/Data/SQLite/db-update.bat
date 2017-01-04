df3405d6ac61e8cfcad1f497be01b5aa4d90f588
gsfbuildbot <support@gridprotectionalliance.org>
2017-01-04 00:07:56 -0500

    Updated GSF dependencies.

--- Source/Dependencies/GSF/Tests/Data/SQLite/db-update.bat
+++ Source/Dependencies/GSF/Tests/Data/SQLite/db-update.bat
@@ -25,40 +25,41 @@
 
 SETLOCAL EnableDelayedExpansion
 
-SET git="%PROGRAMFILES(X86)%\Git\cmd\git.exe"
-SET db[1]="GSFSchema.db"
-SET db[2]="GSFSchema-InitialDataSet.db"
-SET db[3]="GSFSchema-SampleDataSet.db"
-SET script[1]="GSFSchema.sql"
-SET script[2]="InitialDataSet.sql"
-SET script[3]="SampleDataSet.sql"
+IF "%git%" == "" SET git=%PROGRAMFILES(X86)%\Git\cmd\git.exe
+
+SET db[1]=GSFSchema.db
+SET db[2]=GSFSchema-InitialDataSet.db
+SET db[3]=GSFSchema-SampleDataSet.db
+SET script[1]=GSFSchema.sql
+SET script[2]=InitialDataSet.sql
+SET script[3]=SampleDataSet.sql
 
 FOR %%i IN (1 2 3) DO (
     IF NOT "!update!" == "true" (
-        ECHO Checking for pending edits on !script[%%i]!...
-        FOR /f "delims=" %%s IN ('CALL %git% status --short !script[%%i]!') DO SET status=%%s
+        ECHO Checking for pending edits on "!script[%%i]!"...
+        FOR /f "delims=" %%s IN ('CALL "%git%" status --short "!script[%%i]!"') DO SET status=%%s
         IF NOT "!status!" == "" SET update=true
     )
     
     IF NOT "!update!" == "true" (
-        ECHO Checking for changes to !script[%%i]! since last update...
-        FOR /f "tokens=1" %%c IN ('CALL %git% log "--max-count=1" "--pretty=oneline" !db[%%i]!') DO SET commit=%%c
-        FOR /f %%c IN ('CALL %git% log "--max-count=1" "--pretty=oneline" !commit!.. !script[%%i]!') DO SET update=true
+        ECHO Checking for changes to "!script[%%i]!" since last update...
+        FOR /f "tokens=1" %%c IN ('CALL "%git%" log "--max-count=1" "--pretty=oneline" "!db[%%i]!"') DO SET commit=%%c
+        FOR /f %%c IN ('CALL "%git%" log "--max-count=1" "--pretty=oneline" !commit!.. "!script[%%i]!"') DO SET update=true
     )
     
     IF "!update!" == "true" (
-        ECHO Updating !db[%%i]!...
+        ECHO Updating "!db[%%i]!"...
         
-        FOR /f %%v IN ('CALL sqlite3 !db[%%i]! "PRAGMA user_version"') DO SET /a version=%%v+1
+        FOR /f %%v IN ('CALL sqlite3 "!db[%%i]!" "PRAGMA user_version"') DO SET /a version=%%v+1
         
         IF "!prevdb!" == "" (
-            DEL !db[%%i]!
+            DEL "!db[%%i]!"
         ) ELSE (
-            COPY /Y !prevdb! !db[%%i]!
+            COPY /Y "!prevdb!" "!db[%%i]!"
         )
         
-        sqlite3 !db[%%i]! < !script[%%i]!
-        sqlite3 !db[%%i]! "PRAGMA user_version = !version!"
+        sqlite3 "!db[%%i]!" < "!script[%%i]!"
+        sqlite3 "!db[%%i]!" "PRAGMA user_version = !version!"
     )
     
     SET prevdb=!db[%%i]!
