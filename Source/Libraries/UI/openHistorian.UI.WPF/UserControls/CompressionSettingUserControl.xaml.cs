a54342d7dbe2b82ef7f1c89a7486f3008775373c
StephenCWills <swills@gridprotectionalliance.org>
2017-07-26 17:31:20 -0400

    Added WPF control for configuring compression settings.

---
+++ Source/Libraries/UI/openHistorian.UI.WPF/UserControls/CompressionSettingUserControl.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using openHistorian.UI.WPF.ViewModels;
+
+namespace openHistorian.UI.WPF.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CompressionSettingUserControl.xaml
+    /// </summary>
+    public partial class CompressionSettingUserControl : UserControl
+    {
+        #region [ Members ]
+
+        // Fields
+        private CompressionSettingsViewModel m_viewModel;
+        private DataGridColumn m_sortColumn;
+        private string m_sortMemberPath;
+        private ListSortDirection m_sortDirection;
+
+        #endregion
+
+        #region [ Constructors ]
+
+        public CompressionSettingUserControl()
+        {
+            InitializeComponent();
+
+            if (!DesignerProperties.GetIsInDesignMode(this))
+            {
+                m_viewModel = new CompressionSettingsViewModel();
+                m_viewModel.PropertyChanged += ViewModel_PropertyChanged;
+                DataContext = m_viewModel;
+            }
+        }
+
+        #endregion
+
+        #region [ Properties ]
+
+        #endregion
+
+        #region [ Methods ]
+
+        private void MeasurementPagerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MeasurementPagerPopup.IsOpen = true;
+        }
+
+        private void ButtonSelectMeasurement_Click(object sender, RoutedEventArgs e)
+        {
+            m_viewModel.CurrentItem.PointID = MeasurementPager.CurrentItem.PointID;
+            MeasurementPagerPopup.IsOpen = false;
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            MeasurementPagerPopup.IsOpen = false;
+        }
+
+        private void DataGrid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                DataGrid dataGrid = sender as DataGrid;
+                if (dataGrid.SelectedItems.Count > 0)
+                {
+                    if (MessageBox.Show("Are you sure you want to delete " + dataGrid.SelectedItems.Count + " selected item(s)?", "Delete Selected Items", MessageBoxButton.YesNo) == MessageBoxResult.No)
+                        e.Handled = true;
+                }
+            }
+        }
+
+        private void DataGrid_Sorting(object sender, DataGridSortingEventArgs e)
+        {
+            string sortMember;
+
+            if (e.Column.SortMemberPath != m_sortMemberPath)
+                m_sortDirection = ListSortDirection.Ascending;
+            else if (m_sortDirection == ListSortDirection.Ascending)
+                m_sortDirection = ListSortDirection.Descending;
+            else
+                m_sortDirection = ListSortDirection.Ascending;
+
+            m_sortColumn = e.Column;
+            m_sortMemberPath = e.Column.SortMemberPath;
+
+            switch (m_sortMemberPath)
+            {
+                case "OperationDescription":
+                    sortMember = "Operation";
+                    break;
+
+                case "SeverityName":
+                    sortMember = "Severity";
+                    break;
+
+                default:
+                    sortMember = m_sortMemberPath;
+                    break;
+            }
+
+            m_viewModel.SortData(sortMember, m_sortDirection);
+        }
+
+        private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "ItemsSource")
+                Dispatcher.BeginInvoke(new Action(SortDataGrid));
+        }
+
+        private void SortDataGrid()
+        {
+            if ((object)m_sortColumn != null)
+            {
+                m_sortColumn.SortDirection = m_sortDirection;
+                DataGridList.Items.SortDescriptions.Clear();
+                DataGridList.Items.SortDescriptions.Add(new SortDescription(m_sortMemberPath, m_sortDirection));
+                DataGridList.Items.Refresh();
+            }
+        }
+
+        private void GridDetailView_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (m_viewModel.IsNewRecord)
+                DataGridList.SelectedIndex = -1;
+        }
+
+        #endregion
+
+        #region [ Operators ]
+
+        #endregion
+
+        #region [ Static ]
+
+        // Static Fields
+
+        // Static Constructor
+
+        // Static Properties
+
+        // Static Methods
+
+        #endregion
+    }
+}
---
+++ Source/Libraries/UI/openHistorian.UI/DataModels/CompressionSetting.cs
@@ -0,0 +1,329 @@
+﻿//******************************************************************************************************
+//  CompressionSetting.cs - Gbtc
+//
+//  Copyright © 2017, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  07/26/2017 - Stephen C. Wills
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GSF.Data;
+using GSF.TimeSeries.UI;
+using GSF.TimeSeries.UI.DataModels;
+
+namespace openHistorian.UI.DataModels
+{
+    public class CompressionSetting : DataModelBase
+    {
+        #region [ Members ]
+
+        // Fields
+        private int m_pointID;
+        private ulong m_compressionMinTime;
+        private ulong m_compressionMaxTime;
+        private double m_compressionLimit;
+        private Measurement m_referencedMeasurement;
+
+        #endregion
+
+        #region [ Constructors ]
+
+        public CompressionSetting()
+        {
+            IsNew = true;
+        }
+
+        #endregion
+
+        #region [ Properties ]
+        
+        public int PointID
+        {
+            get
+            {
+                return m_pointID;
+            }
+            set
+            {
+                m_pointID = value;
+                m_referencedMeasurement = null;
+                OnPropertyChanged(nameof(PointID));
+                OnPropertyChanged(nameof(PointTag));
+            }
+        }
+
+        public ulong CompressionMinTime
+        {
+            get
+            {
+                return m_compressionMinTime;
+            }
+            set
+            {
+                m_compressionMinTime = value;
+                OnPropertyChanged(nameof(CompressionMinTime));
+            }
+        }
+
+        public ulong CompressionMaxTime
+        {
+            get
+            {
+                return m_compressionMaxTime;
+            }
+            set
+            {
+                m_compressionMaxTime = value;
+                OnPropertyChanged(nameof(CompressionMaxTime));
+            }
+        }
+
+        public double CompressionLimit
+        {
+            get
+            {
+                return m_compressionLimit;
+            }
+            set
+            {
+                m_compressionLimit = value;
+                OnPropertyChanged(nameof(CompressionLimit));
+            }
+        }
+
+        public string PointTag
+        {
+            get
+            {
+                return ReferencedMeasurement?.PointTag;
+            }
+        }
+
+        public bool IsNew
+        {
+            get;
+            set;
+        }
+
+        private Measurement ReferencedMeasurement
+        {
+            get
+            {
+                return m_referencedMeasurement ?? GetReferencedMeasurement();
+            }
+        }
+
+        #endregion
+
+        #region [ Methods ]
+
+        private Measurement GetReferencedMeasurement()
+        {
+            return Measurement.GetMeasurement(null, $"WHERE PointID = {m_pointID}");
+        }
+
+        #endregion
+
+        #region [ Operators ]
+
+        #endregion
+
+        #region [ Static ]
+
+        // Static Methods
+
+        /// <summary>
+        /// Loads <see cref="CompressionSetting"/> IDs as an <see cref="IList{T}"/>.
+        /// </summary>        
+        /// <param name="database"><see cref="AdoDataConnection"/> to connection to database.</param>
+        /// <param name="sortMember">The field to sort by.</param>
+        /// <param name="sortDirection"><c>ASC</c> or <c>DESC</c> for ascending or descending respectively.</param>
+        /// <returns>Collection of <see cref="Int32"/>.</returns>
+        public static IList<int> LoadKeys(AdoDataConnection database, string sortMember = "", string sortDirection = "")
+        {
+            bool createdConnection = false;
+
+            try
+            {
+                createdConnection = CreateConnection(ref database);
+
+                IList<int> compressionSettingList = new List<int>();
+                string sortClause = string.Empty;
+                DataTable compressionSettingTable;
+
+                if (!string.IsNullOrEmpty(sortMember))
+                    sortClause = string.Format("ORDER BY {0} {1}", sortMember, sortDirection);
+
+                compressionSettingTable = database.RetrieveData($"SELECT PointID FROM CompressionSetting {sortClause}");
+
+                foreach (DataRow row in compressionSettingTable.Rows)
+                    compressionSettingList.Add(row.ConvertField<int>("PointID"));
+
+                return compressionSettingList;
+            }
+            finally
+            {
+                if (createdConnection && database != null)
+                    database.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Loads <see cref="CompressionSetting"/> information as an <see cref="ObservableCollection{T}"/> style list.
+        /// </summary>        
+        /// <param name="database"><see cref="AdoDataConnection"/> to connection to database.</param>
+        /// <param name="keys">Keys of the compression settings to be loaded from the database.</param>
+        /// <returns>Collection of <see cref="CompressionSetting"/>.</returns>
+        public static ObservableCollection<CompressionSetting> Load(AdoDataConnection database, IList<int> keys)
+        {
+            bool createdConnection = false;
+
+            try
+            {
+                createdConnection = CreateConnection(ref database);
+
+                string commaSeparatedKeys;
+
+                CompressionSetting[] compressionSettingList = null;
+                DataTable compressionSettingTable;
+                int pointID;
+
+                if ((object)keys != null && keys.Count > 0)
+                {
+                    commaSeparatedKeys = keys.Select(key => "" + key.ToString() + "").Aggregate((str1, str2) => str1 + "," + str2);
+
+                    compressionSettingTable = database.RetrieveData(
+                        $"SELECT PointID, CompressionMinTime, CompressionMaxTime, CompressionLimit " +
+                        $"FROM CompressionSetting WHERE PointID IN ({commaSeparatedKeys})");
+
+                    compressionSettingList = new CompressionSetting[compressionSettingTable.Rows.Count];
+
+                    foreach (DataRow row in compressionSettingTable.Rows)
+                    {
+                        pointID = row.ConvertField<int>(nameof(PointID));
+
+                        compressionSettingList[keys.IndexOf(pointID)] = new CompressionSetting()
+                        {
+                            PointID = pointID,
+                            CompressionMinTime = row.ConvertField<ulong>(nameof(CompressionMinTime)),
+                            CompressionMaxTime = row.ConvertField<ulong>(nameof(CompressionMaxTime)),
+                            CompressionLimit = row.ConvertField<double>(nameof(CompressionLimit)),
+                            IsNew = false
+                        };
+                    }
+                }
+
+                return new ObservableCollection<CompressionSetting>(compressionSettingList ?? new CompressionSetting[0]);
+            }
+            finally
+            {
+                if (createdConnection && database != null)
+                    database.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Saves <see cref="CompressionSetting"/> information to database.
+        /// </summary>
+        /// <param name="database"><see cref="AdoDataConnection"/> to connection to database.</param>
+        /// <param name="compressionSetting">Information about <see cref="CompressionSetting"/>.</param>        
+        /// <returns>String, for display use, indicating success.</returns>
+        public static string Save(AdoDataConnection database, CompressionSetting compressionSetting)
+        {
+            bool createdConnection = false;
+
+            try
+            {
+                createdConnection = CreateConnection(ref database);
+
+                string successMessage = "CompressionSetting information saved successfully";
+
+                if (compressionSetting.IsNew)
+                {
+                    database.ExecuteNonQuery(
+                        $"INSERT INTO CompressionSetting ({nameof(PointID)}, {nameof(CompressionMinTime)}, {nameof(CompressionMaxTime)}, {nameof(CompressionLimit)}) " +
+                        $"VALUES({{0}}, {{1}}, {{2}}, {{3}})", compressionSetting.PointID, compressionSetting.CompressionMinTime, compressionSetting.CompressionMaxTime, compressionSetting.CompressionLimit);
+
+                    compressionSetting.IsNew = false;
+                }
+                else
+                {
+                    database.ExecuteNonQuery(
+                        $"UPDATE CompressionSetting SET " +
+                        $"    {nameof(CompressionMinTime)} = {{0}}, " +
+                        $"    {nameof(CompressionMaxTime)} = {{1}}, " +
+                        $"    {nameof(CompressionLimit)} = {{2}} " +
+                        $"WHERE {nameof(PointID)} = {{3}}",
+                        compressionSetting.CompressionMinTime, compressionSetting.CompressionMaxTime, compressionSetting.CompressionLimit, compressionSetting.PointID);
+                }
+
+                try
+                {
+                    CommonFunctions.SendCommandToService("ReloadConfig");
+                }
+                catch (Exception ex)
+                {
+                    CommonFunctions.LogException(database, "CompressionSetting Save", ex);
+                }
+
+                return successMessage;
+            }
+            finally
+            {
+                if (createdConnection && database != null)
+                    database.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Deletes specified <see cref="CompressionSetting"/> record from database.
+        /// </summary>
+        /// <param name="database"><see cref="AdoDataConnection"/> to connection to database.</param>
+        /// <param name="pointID">ID of the record to be deleted.</param>
+        /// <returns>String, for display use, indicating success.</returns>
+        public static string Delete(AdoDataConnection database, int pointID)
+        {
+            bool createdConnection = false;
+
+            try
+            {
+                createdConnection = CreateConnection(ref database);
+
+                // Setup current user context for any delete triggers
+                CommonFunctions.SetCurrentUserContext(database);
+                database.ExecuteNonQuery("DELETE FROM CompressionSetting WHERE PointID = {0}", pointID);
+                CommonFunctions.SendCommandToService("ReloadConfig");
+
+                return "CompressionSetting deleted successfully";
+            }
+            finally
+            {
+                if (createdConnection && database != null)
+                    database.Dispose();
+            }
+        }
+
+        #endregion
+    }
+}
