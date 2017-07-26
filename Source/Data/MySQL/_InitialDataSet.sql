2b0e96cf3a2c5aa2090974b5462a14925bf52b5f
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2017-07-26 14:05:39 -0400

    Added configuration entity to the initial database values for making compression settings available to the adapters as part of the core configuration

--- Source/Data/MySQL/_InitialDataSet.sql
+++ Source/Data/MySQL/_InitialDataSet.sql
@@ -1,1 +1,3 @@
 INSERT INTO Protocol(Acronym, Name, Type, Category, AssemblyName, TypeName, LoadOrder) VALUES('Modbus', 'Modbus Poller', 'Measurement', 'Device', 'ModbusAdapters.dll', 'ModbusAdapters.ModbusPoller', 13);
+
+INSERT INTO ConfigurationEntity(SourceName, RuntimeName, Description, LoadOrder, Enabled) VALUES('CompressionSetting', 'CompressionSetting', 'Defines information about measurement compression settings', 19, 1);
