1fb600f201e64d57c5466efe5e6eea988a848ff6
Steven Chisholm <steven@chizware.com>
2017-07-27 09:53:19 -0500

    Archive Integrity Checker: Added a quick scan option.

--- Source/Tools/Archive Integrity Checker/FrmMain.Designer.cs
+++ Source/Tools/Archive Integrity Checker/FrmMain.Designer.cs
@@ -32,6 +32,9 @@
             this.txtResults = new System.Windows.Forms.TextBox();
             this.lblProgress = new System.Windows.Forms.Label();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.chkQuickScan = new System.Windows.Forms.CheckBox();
+            this.lblMBToScan = new System.Windows.Forms.Label();
+            this.txtMBToScan = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // BtnBrowse
@@ -49,11 +52,13 @@
             this.txtResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtResults.Location = new System.Drawing.Point(12, 41);
+            this.txtResults.Location = new System.Drawing.Point(12, 66);
             this.txtResults.Multiline = true;
             this.txtResults.Name = "txtResults";
-            this.txtResults.Size = new System.Drawing.Size(458, 257);
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResults.Size = new System.Drawing.Size(458, 232);
             this.txtResults.TabIndex = 1;
+            this.txtResults.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtResults_KeyDown);
             // 
             // lblProgress
             // 
@@ -74,11 +79,42 @@
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // chkQuickScan
+            // 
+            this.chkQuickScan.AutoSize = true;
+            this.chkQuickScan.Location = new System.Drawing.Point(12, 43);
+            this.chkQuickScan.Name = "chkQuickScan";
+            this.chkQuickScan.Size = new System.Drawing.Size(82, 17);
+            this.chkQuickScan.TabIndex = 4;
+            this.chkQuickScan.Text = "Quick Scan";
+            this.chkQuickScan.UseVisualStyleBackColor = true;
+            this.chkQuickScan.CheckedChanged += new System.EventHandler(this.chkQuickScan_CheckedChanged);
+            // 
+            // lblMBToScan
+            // 
+            this.lblMBToScan.AutoSize = true;
+            this.lblMBToScan.Location = new System.Drawing.Point(152, 43);
+            this.lblMBToScan.Name = "lblMBToScan";
+            this.lblMBToScan.Size = new System.Drawing.Size(67, 13);
+            this.lblMBToScan.TabIndex = 5;
+            this.lblMBToScan.Text = "MB To Scan";
+            // 
+            // txtMBToScan
+            // 
+            this.txtMBToScan.Location = new System.Drawing.Point(225, 41);
+            this.txtMBToScan.Name = "txtMBToScan";
+            this.txtMBToScan.Size = new System.Drawing.Size(40, 20);
+            this.txtMBToScan.TabIndex = 6;
+            this.txtMBToScan.Text = "1";
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(482, 310);
+            this.Controls.Add(this.txtMBToScan);
+            this.Controls.Add(this.lblMBToScan);
+            this.Controls.Add(this.chkQuickScan);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.lblProgress);
             this.Controls.Add(this.txtResults);
@@ -96,6 +132,9 @@
         private System.Windows.Forms.TextBox txtResults;
         private System.Windows.Forms.Label lblProgress;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.CheckBox chkQuickScan;
+        private System.Windows.Forms.Label lblMBToScan;
+        private System.Windows.Forms.TextBox txtMBToScan;
     }
 }
 
--- Source/Tools/Archive Integrity Checker/FrmMain.cs
+++ Source/Tools/Archive Integrity Checker/FrmMain.cs
@@ -23,9 +23,23 @@ namespace Archive_Integrity_Checker
         }
 
         private volatile bool m_quit;
+        private bool m_shouldQuickScan;
+        private double m_mbToScan;
+        private int m_filesScanned;
+        private int m_filesToScan;
 
         private void BtnBrowse_Click(object sender, EventArgs e)
         {
+            m_filesScanned = 0;
+            m_shouldQuickScan = chkQuickScan.Checked;
+            if (m_shouldQuickScan)
+            {
+                if (!double.TryParse(txtMBToScan.Text, out m_mbToScan))
+                {
+                    MessageBox.Show("Enter a valid decimal for MB To Scan");
+                    return;
+                }
+            }
             m_quit = false;
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("Result\tName");
@@ -36,16 +50,21 @@ namespace Archive_Integrity_Checker
 
                 if (dlg.ShowDialog() == DialogResult.OK)
                 {
+                    m_filesToScan = dlg.FileNames.Length;
                     foreach (var file in dlg.FileNames)
                     {
                         sb.AppendLine($"{TestFile(file)}\t{file}");
                         txtResults.Text = sb.ToString();
                         if (m_quit)
                             return;
+                        lblProgress.Text = $"Completed {m_filesScanned} of {m_filesToScan} files. Current File: 100%";
+                        Application.DoEvents();
+                        m_filesScanned++;
                     }
                 }
-
             }
+
+            lblProgress.Text = "Done";
         }
 
         private unsafe string TestFile(string fileName)
@@ -67,7 +86,17 @@ namespace Archive_Integrity_Checker
                     {
                         stream.Position = header.HeaderBlockCount * blockSize;
 
-                        for (uint x = header.HeaderBlockCount; x <= header.LastAllocatedBlock; x++)
+                        uint startingBlock = header.HeaderBlockCount;
+                        if (m_shouldQuickScan)
+                        {
+                            uint blocksToScan = (uint)Math.Ceiling(m_mbToScan * 1024 * 1024 / blockSize);
+                            if (blocksToScan < header.LastAllocatedBlock)
+                            {
+                                startingBlock = Math.Max(header.HeaderBlockCount, header.LastAllocatedBlock - blocksToScan);
+                            }
+                        }
+
+                        for (uint x = startingBlock; x <= header.LastAllocatedBlock; x++)
                         {
                             long checksum1;
                             int checksum2;
@@ -94,7 +123,7 @@ namespace Archive_Integrity_Checker
                             if (lastReport.ElapsedSeconds() > .25)
                             {
                                 lastReport = ShortTime.Now;
-                                lblProgress.Text = (stream.Position / (double)stream.Length).ToString("0.0%");
+                                lblProgress.Text = $"Completed {m_filesScanned} of {m_filesToScan} files. Current File: {(stream.Position / (double)stream.Length).ToString("0.0%")}";
                                 Application.DoEvents();
                             }
                         }
@@ -118,6 +147,22 @@ namespace Archive_Integrity_Checker
         private void btnCancel_Click(object sender, EventArgs e)
         {
             m_quit = true;
+        }
+
+        private void chkQuickScan_CheckedChanged(object sender, EventArgs e)
+        {
+            lblMBToScan.Visible = chkQuickScan.Checked;
+            txtMBToScan.Visible = chkQuickScan.Checked;
+        }
+
+        private void txtResults_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && (e.KeyCode == Keys.A))
+            {
+                if (sender != null)
+                    ((TextBox)sender).SelectAll();
+                e.Handled = true;
+            }
         }
     }
 }
