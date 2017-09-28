b4d3fc55df548fb3d586005d195e54ce4806cd92
StephenCWills <swills@gridprotectionalliance.org>
2017-09-28 15:33:38 -0400

    Added web API to manipulate the ImportedMeasurement table.

---
+++ Source/Libraries/Adapters/openHistorian.Adapters/Model/ImportedMeasurement.cs
@@ -0,0 +1,239 @@
+﻿//******************************************************************************************************
+//  ImportedMeasurement.cs - Gbtc
+//
+//  Copyright © 2017, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  09/27/2017 - Stephen C. Wills
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+#pragma warning disable 1591
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Transactions;
+using System.Web.Http;
+using GSF.Configuration;
+using GSF.Data;
+using GSF.Data.Model;
+
+namespace openHistorian.Model
+{
+    public class ImportedMeasurement
+    {
+        public Guid? NodeID { get; set; }
+
+        public Guid? SourceNodeID { get; set; }
+
+        public Guid? SignalID { get; set; }
+
+        [StringLength(200)]
+        public string Source { get; set; }
+
+        public int PointID { get; set; }
+
+        [StringLength(200)]
+        public string PointTag { get; set; }
+
+        [StringLength(200)]
+        public string AlternateTag { get; set; }
+
+        [StringLength(4)]
+        public string SignalTypeAcronym { get; set; }
+
+        [StringLength(200)]
+        public string SignalReference { get; set; }
+
+        public int? FramesPerSecond { get; set; }
+
+        [StringLength(200)]
+        public string ProtocolAcronym { get; set; }
+
+        [StringLength(200)]
+        [DefaultValue("Frame")]
+        public string ProtocolType { get; set; }
+
+        public int? PhasorID { get; set; }
+
+        public char? PhasorType { get; set; }
+
+        public char? Phase { get; set; }
+
+        [DefaultValue(0.0D)]
+        public double Adder { get; set; }
+
+        [DefaultValue(1.0D)]
+        public double Multiplier { get; set; }
+
+        [StringLength(200)]
+        public string CompanyAcronym { get; set; }
+
+        public double? Longitude { get; set; }
+
+        public double? Latitude { get; set; }
+
+        public string Description { get; set; }
+
+        [DefaultValue(false)]
+        public bool Enabled { get; set; }
+    }
+
+    public class ImportedMeasurementsController : ApiController
+    {
+        #region [ Properties ]
+
+        private Guid NodeID
+        {
+            get
+            {
+                ConfigurationFile configurationFile = ConfigurationFile.Current;
+                CategorizedSettingsElementCollection systemSettings = configurationFile.Settings["systemSettings"];
+                string nodeIDSetting = systemSettings["NodeID"].Value;
+                Guid nodeID;
+
+                if (!Guid.TryParse(nodeIDSetting, out nodeID))
+                    return Guid.Empty;
+
+                return nodeID;
+            }
+        }
+
+        private string Source
+        {
+            get
+            {
+                string username = User.Identity.Name;
+
+                return new string(username
+                    .ToUpper()
+                    .Select(c => char.IsLetterOrDigit(c) ? c : '_')
+                    .ToArray());
+            }
+        }
+
+        #endregion
+
+        #region [ Methods ]
+
+        [HttpGet]
+        public IEnumerable<ImportedMeasurement> FindAll()
+        {
+            return QueryImportedMeasurements();
+        }
+
+        [HttpGet]
+        public IEnumerable<ImportedMeasurement> FindByID(int id)
+        {
+            return QueryImportedMeasurements(new RecordRestriction("PointID = {0}", id));
+        }
+
+        [HttpGet]
+        public IEnumerable<ImportedMeasurement> FindByPointTag(string id)
+        {
+            return QueryImportedMeasurements(new RecordRestriction("PointTag = {0}", id));
+        }
+
+        [HttpGet]
+        public IEnumerable<ImportedMeasurement> FindByAlternateTag(string id)
+        {
+            return QueryImportedMeasurements(new RecordRestriction("AlternateTag = {0}", id));
+        }
+
+        [HttpPost]
+        public void ImportMeasurement(ImportedMeasurement measurement)
+        {
+            CreateOrUpdate(measurement);
+        }
+
+        [HttpDelete]
+        public void DeleteMeasurement(int id)
+        {
+            int pointID = id;
+
+            using (AdoDataConnection connection = CreateDbConnection())
+            {
+                TableOperations<ImportedMeasurement> importedMeasurementTable = new TableOperations<ImportedMeasurement>(connection);
+
+                RecordRestriction recordRestriction =
+                    new RecordRestriction("NodeID = {0}", NodeID) &
+                    new RecordRestriction("Source = {0}", Source) &
+                    new RecordRestriction("PointID = {0}", pointID);
+
+                importedMeasurementTable.DeleteRecord(recordRestriction);
+            }
+        }
+
+        private IEnumerable<ImportedMeasurement> QueryImportedMeasurements(RecordRestriction recordRestriction = null)
+        {
+            using (AdoDataConnection connection = CreateDbConnection())
+            {
+                TableOperations<ImportedMeasurement> importedMeasurementTable = new TableOperations<ImportedMeasurement>(connection);
+
+                RecordRestriction queryRestriction =
+                    new RecordRestriction("NodeID = {0}", NodeID) &
+                    new RecordRestriction("Source = {0}", Source);
+
+                if ((object)recordRestriction != null)
+                    queryRestriction &= recordRestriction;
+
+                return importedMeasurementTable.QueryRecords(queryRestriction);
+            }
+        }
+
+        private void CreateOrUpdate(ImportedMeasurement measurement)
+        {
+            TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required;
+
+            TransactionOptions transactionOptions = new TransactionOptions()
+            {
+                IsolationLevel = IsolationLevel.ReadCommitted,
+                Timeout = TransactionManager.MaximumTimeout
+            };
+
+            measurement.NodeID = NodeID;
+            measurement.Source = Source;
+
+            using (TransactionScope transactionScope = new TransactionScope(transactionScopeOption, transactionOptions))
+            using (AdoDataConnection connection = CreateDbConnection())
+            {
+                TableOperations<ImportedMeasurement> importedMeasurementTable = new TableOperations<ImportedMeasurement>(connection);
+
+                RecordRestriction recordRestriction =
+                    new RecordRestriction("NodeID = {0}", measurement.NodeID) &
+                    new RecordRestriction("Source = {0}", measurement.Source) &
+                    new RecordRestriction("PointID = {0}", measurement.PointID);
+
+                int measurementCount = importedMeasurementTable.UpdateRecord(measurement, recordRestriction);
+
+                if (measurementCount == 0)
+                    importedMeasurementTable.AddNewRecord(measurement);
+
+                transactionScope.Complete();
+            }
+        }
+
+        private AdoDataConnection CreateDbConnection()
+        {
+            return new AdoDataConnection("systemSettings");
+        }
+
+        #endregion
+    }
+}
