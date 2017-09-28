034f50862fca72b55d08ab95e7ab4f5273ee0fb1
StephenCWills <swills@gridprotectionalliance.org>
2017-09-28 15:38:13 -0400

    Fixed default anonymous resource expression to exclude the web API for manipulating imported measurements.

--- Source/Applications/openHistorian/openHistorian/ServiceHost.cs
+++ Source/Applications/openHistorian/openHistorian/ServiceHost.cs
@@ -158,7 +158,7 @@ namespace openHistorian
             CategorizedSettingsElementCollection securityProvider = ConfigurationFile.Current.Settings["securityProvider"];
 
             // Define set of default anonymous web resources for this site
-            const string DefaultAnonymousResourceExpression = "^/@|^/Scripts/|^/Content/|^/Images/|^/fonts/|^/api/|^/instance/|^/favicon.ico$";
+            const string DefaultAnonymousResourceExpression = "^/@|^/Scripts/|^/Content/|^/Images/|^/fonts/|^/api/(?!importedmeasurements)|^/instance/|^/favicon.ico$";
 
             systemSettings.Add("CompanyName", "Grid Protection Alliance", "The name of the company who owns this instance of the openHistorian.");
             systemSettings.Add("CompanyAcronym", "GPA", "The acronym representing the company who owns this instance of the openHistorian.");
