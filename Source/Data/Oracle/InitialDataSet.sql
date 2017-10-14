4927078fc23ca4dfae0fdcdfa632ae2bebbdbb1d
gsfbuildbot <support@gridprotectionalliance.org>
2017-10-14 00:01:58 -0400

    Updated GSF dependencies.

--- Source/Data/Oracle/InitialDataSet.sql
+++ Source/Data/Oracle/InitialDataSet.sql
@@ -238,5 +238,6 @@ INSERT INTO VendorDevice(VendorID, Name, Description, URL) VALUES(14, 'SIMEAS R-
 INSERT INTO VendorDevice(VendorID, Name, Description, URL) VALUES(15, 'ePDC', 'ePDC &' || ' eSPDC', 'http://www.electricpowergroup.com/solutions/epdc/index.html');
  
 INSERT INTO Protocol(Acronym, Name, Type, Category, AssemblyName, TypeName, LoadOrder) VALUES('Modbus', 'Modbus Poller', 'Measurement', 'Device', 'ModbusAdapters.dll', 'ModbusAdapters.ModbusPoller', 13);
+INSERT INTO Protocol(Acronym, Name, Type, Category, AssemblyName, TypeName, LoadOrder) VALUES('COMTRADE', 'COMTRADE Import', 'Measurement', 'Imported', 'TestingAdapters.dll', 'TestingAdapters.VirtualInputAdapter', 14);
 
 INSERT INTO ConfigurationEntity(SourceName, RuntimeName, Description, LoadOrder, Enabled) VALUES('NodeCompressionSetting', 'CompressionSettings', 'Defines information about measurement compression settings', 19, 1);
