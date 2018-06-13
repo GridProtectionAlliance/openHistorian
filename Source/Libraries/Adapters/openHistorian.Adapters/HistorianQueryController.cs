7fad2230974cde772122ede869acf44858a05aa2
StephenCWills <swills@gridprotectionalliance.org>
2018-06-13 17:28:19 -0400

    Added the HistorianQueryController and converted SignalR calls to query this new controller.

---
+++ Source/Libraries/Adapters/openHistorian.Adapters/HistorianQueryController.cs
@@ -0,0 +1,117 @@
+﻿//******************************************************************************************************
+//  HistorianQueryController.cs - Gbtc
+//
+//  Copyright © 2018, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  06/13/2018 - Stephen C. Wills
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using GSF.Snap.Services;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using openHistorian.Model;
+using openHistorian.Net;
+using CancellationToken = GSF.Threading.CancellationToken;
+
+namespace openHistorian.Adapters
+{
+    /// <summary>
+    /// Web API controller for querying historian data.
+    /// </summary>
+    public class HistorianQueryController : ApiController
+    {
+        private class QueryParameters
+        {
+            public string instanceName;
+            public DateTime startTime;
+            public DateTime stopTime;
+            public ulong[] measurementIDs;
+            public Resolution resolution;
+            public int seriesLimit;
+            public bool forceLimit;
+            public TimestampType timestampType = TimestampType.UnixMilliseconds;
+        }
+
+        private CancellationToken m_readCancellationToken;
+
+        /// <summary>
+        /// Read historian data from server.
+        /// </summary>
+        [HttpPost]
+        public async Task<IEnumerable<TrendValue>> GetHistorianData()
+        {
+            QueryParameters queryParameters;
+
+            using (Stream contentStream = await Request.Content.ReadAsStreamAsync())
+            using (StreamReader contentReader = new StreamReader(contentStream))
+            using (JsonTextReader jsonReader = new JsonTextReader(contentReader))
+            {
+                JObject jsonObject = await JObject.LoadAsync(jsonReader);
+                queryParameters = jsonObject.ToObject<QueryParameters>();
+            }
+
+            string instanceName = queryParameters.instanceName;
+            DateTime startTime = queryParameters.startTime;
+            DateTime stopTime = queryParameters.stopTime;
+            ulong[] measurementIDs = queryParameters.measurementIDs;
+            Resolution resolution = queryParameters.resolution;
+            int seriesLimit = queryParameters.seriesLimit;
+            bool forceLimit = queryParameters.forceLimit;
+
+            // Cancel any running operation
+            CancellationToken cancellationToken = new CancellationToken();
+            Interlocked.Exchange(ref m_readCancellationToken, cancellationToken)?.Cancel();
+
+            SnapServer server = GetServer(instanceName)?.Host;
+            IEnumerable<TrendValue> values = TrendValueAPI.GetHistorianData(server, instanceName, startTime, stopTime, measurementIDs, resolution, seriesLimit, forceLimit, cancellationToken);
+
+            switch (queryParameters.timestampType)
+            {
+                case TimestampType.Ticks:
+                    return values.Select(value =>
+                    {
+                        value.Timestamp = value.Timestamp * 10000.0D + 621355968000000000.0D;
+                        return value;
+                    });
+                case TimestampType.UnixSeconds:
+                    return values.Select(value =>
+                    {
+                        value.Timestamp = value.Timestamp / 1000.0D;
+                        return value;
+                    });
+                default:
+                    return values;
+            }
+        }
+
+        private HistorianServer GetServer(string instanceName)
+        {
+            if (LocalOutputAdapter.Instances.TryGetValue(instanceName, out LocalOutputAdapter historianAdapter))
+                return historianAdapter?.Server;
+
+            return null;
+        }
+    }
+}
