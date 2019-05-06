6a20b3d8a0024dd552dc57c9653a163ab32586ad
gsfbuildbot <support@gridprotectionalliance.org>
2019-05-06 15:06:40 -0400

    openHistorian: Version change for build v2.6.4.0-master.

--- Source/Applications/openHistorian/openHistorianConsole/Properties/AssemblyInfo.cs
+++ Source/Applications/openHistorian/openHistorianConsole/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
 
-[assembly: AssemblyVersion("2.6.3.0")]
+[assembly: AssemblyVersion("2.6.4.0")]
 
 // Informational attributes.
 
