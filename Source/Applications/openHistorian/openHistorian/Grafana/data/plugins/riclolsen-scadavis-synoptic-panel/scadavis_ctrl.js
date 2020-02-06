3fdc81365901788df8e125fed1112aa64038b22f
Christoph Lackner <clackner@gridprotectionalliance.org>
2020-02-06 17:57:00 -0500

    Updated Scadavis Panel to newest version

--- Source/Applications/openHistorian/openHistorian/Grafana/data/plugins/riclolsen-scadavis-synoptic-panel/scadavis_ctrl.js
+++ Source/Applications/openHistorian/openHistorian/Grafana/data/plugins/riclolsen-scadavis-synoptic-panel/scadavis_ctrl.js
@@ -164,11 +164,19 @@ System.register(['app/plugins/sdk', 'lodash', 'app/core/utils/kbn', 'app/core/ti
             this.series = dataList.map(this.seriesHandler.bind(this));
             this.data = this.parseSeries(this.series);
             this.render(this.data);
+
+            var svgurl = this.panel.svgurl;
+            try {
+              svgurl = this.templateSrv.replace(this.panel.svgurl, this.panel.scopedVars, 'html');
+            } catch (e) {
+              console.log('panel error: ', e);
+            }
+
             if (typeof this.$panelContainer != 'undefined') {
               var svelem = this.$panelContainer[0].querySelector('.scadavis-panel__chart');
 
               if (svelem && typeof svelem.svgraph === "undefined") {
-                svelem.svgraph = new scadavis({ svgurl: this.panel.svgurl,
+                svelem.svgraph = new scadavis({ svgurl: svgurl,
                   container: svelem,
                   iframeparams: 'frameborder="0" width="' + svelem.clientWidth + '" height="' + svelem.clientHeight + '"'
                 });
@@ -188,8 +196,9 @@ System.register(['app/plugins/sdk', 'lodash', 'app/core/utils/kbn', 'app/core/ti
                 //   svelem.svgraph.loadURL(this.panel.svgurl);
                 //   }
                 //else
-                if (svelem.svgraph.svgurl != this.panel.svgurl) {
-                  svelem.svgraph.loadURL(this.panel.svgurl);
+
+                if (svelem.svgraph.svgurl != svgurl) {
+                  svelem.svgraph.loadURL(svgurl);
                 }
                 svelem.svgraph.enableTools(this.panel.showZoomPan, this.panel.showZoomPan);
                 for (var i = 0; i < this.data.length; i++) {
--- Source/Applications/openHistorian/openHistorian/Grafana/data/plugins/riclolsen-scadavis-synoptic-panel/src/module.js
+++
@@ -1,9 +0,0 @@
-import { SCADAvisCtrl } from './scadavis_ctrl';
-import { loadPluginCss } from 'app/plugins/sdk';
-
-loadPluginCss({
-  dark: 'plugins/scadavis-synoptic-panel/css/scadavis.dark.css',
-  light: 'plugins/scadavis-synoptic-panel/css/scadavis.light.css',
-});
-
-export { SCADAvisCtrl as PanelCtrl };
