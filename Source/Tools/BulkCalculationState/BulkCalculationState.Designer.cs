285831e9c9644d65aa1044b4693b40348a6a7ece
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2020-04-08 13:39:10 -0500

    Added bulk-calculation state tool

---
+++ Source/Tools/BulkCalculationState/BulkCalculationState.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace BulkCalculationState
+{
+    partial class BulkCalculationState
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBoxDevices = new System.Windows.Forms.CheckedListBox();
+            this.checkBoxSelectAll = new System.Windows.Forms.CheckBox();
+            this.buttonEnableSelected = new System.Windows.Forms.Button();
+            this.checkBoxSelect = new System.Windows.Forms.CheckBox();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.labelSelected = new System.Windows.Forms.Label();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkedListBoxDevices
+            // 
+            this.checkedListBoxDevices.CheckOnClick = true;
+            this.checkedListBoxDevices.FormattingEnabled = true;
+            this.checkedListBoxDevices.Location = new System.Drawing.Point(12, 67);
+            this.checkedListBoxDevices.Name = "checkedListBoxDevices";
+            this.checkedListBoxDevices.Size = new System.Drawing.Size(329, 364);
+            this.checkedListBoxDevices.TabIndex = 4;
+            this.checkedListBoxDevices.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBoxDevices_ItemCheck);
+            // 
+            // checkBoxSelectAll
+            // 
+            this.checkBoxSelectAll.AutoSize = true;
+            this.checkBoxSelectAll.Checked = true;
+            this.checkBoxSelectAll.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxSelectAll.Location = new System.Drawing.Point(15, 11);
+            this.checkBoxSelectAll.Name = "checkBoxSelectAll";
+            this.checkBoxSelectAll.Size = new System.Drawing.Size(123, 17);
+            this.checkBoxSelectAll.TabIndex = 0;
+            this.checkBoxSelectAll.Text = "Select / Unselect &All";
+            this.checkBoxSelectAll.UseVisualStyleBackColor = true;
+            this.checkBoxSelectAll.CheckedChanged += new System.EventHandler(this.checkBoxSelectAll_CheckedChanged);
+            // 
+            // buttonEnableSelected
+            // 
+            this.buttonEnableSelected.Location = new System.Drawing.Point(378, 12);
+            this.buttonEnableSelected.Name = "buttonEnableSelected";
+            this.buttonEnableSelected.Size = new System.Drawing.Size(111, 29);
+            this.buttonEnableSelected.TabIndex = 5;
+            this.buttonEnableSelected.Text = "&Enable Selected";
+            this.buttonEnableSelected.UseVisualStyleBackColor = true;
+            this.buttonEnableSelected.Click += new System.EventHandler(this.buttonEnableSelected_Click);
+            // 
+            // checkBoxSelect
+            // 
+            this.checkBoxSelect.AutoSize = true;
+            this.checkBoxSelect.Checked = true;
+            this.checkBoxSelect.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxSelect.Location = new System.Drawing.Point(15, 35);
+            this.checkBoxSelect.Name = "checkBoxSelect";
+            this.checkBoxSelect.Size = new System.Drawing.Size(109, 17);
+            this.checkBoxSelect.TabIndex = 1;
+            this.checkBoxSelect.Text = "&Select / Unselect";
+            this.checkBoxSelect.UseVisualStyleBackColor = true;
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(130, 33);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(150, 20);
+            this.textBoxSearch.TabIndex = 2;
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.Location = new System.Drawing.Point(276, 31);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(65, 23);
+            this.buttonFind.TabIndex = 3;
+            this.buttonFind.Text = "&Find";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // labelSelected
+            // 
+            this.labelSelected.AutoSize = true;
+            this.labelSelected.Location = new System.Drawing.Point(403, 76);
+            this.labelSelected.Name = "labelSelected";
+            this.labelSelected.Size = new System.Drawing.Size(86, 13);
+            this.labelSelected.TabIndex = 6;
+            this.labelSelected.Tag = "Selected: {0:N0}";
+            this.labelSelected.Text = "Selected: {0:N0}";
+            this.labelSelected.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.Location = new System.Drawing.Point(421, 53);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(68, 13);
+            this.labelTotal.TabIndex = 7;
+            this.labelTotal.Tag = "Total: {0:N0}";
+            this.labelTotal.Text = "Total: {0:N0}";
+            this.labelTotal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // BulkCalculationState
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 443);
+            this.Controls.Add(this.labelTotal);
+            this.Controls.Add(this.labelSelected);
+            this.Controls.Add(this.buttonFind);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.checkBoxSelect);
+            this.Controls.Add(this.buttonEnableSelected);
+            this.Controls.Add(this.checkBoxSelectAll);
+            this.Controls.Add(this.checkedListBoxDevices);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "BulkCalculationState";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bulk Calculation State";
+            this.Load += new System.EventHandler(this.BulkCalculationState_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBoxDevices;
+        private System.Windows.Forms.CheckBox checkBoxSelectAll;
+        private System.Windows.Forms.Button buttonEnableSelected;
+        private System.Windows.Forms.CheckBox checkBoxSelect;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.Label labelSelected;
+        private System.Windows.Forms.Label labelTotal;
+    }
+}
+
---
+++ Source/Tools/BulkCalculationState/BulkCalculationState.cs
@@ -0,0 +1,179 @@
+﻿//******************************************************************************************************
+//  BulkCalculationState.cs - Gbtc
+//
+//  Copyright © 2020, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  02/25/2020 - rcarroll
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using BulkCalculationState.Model;
+using GSF.Data;
+using GSF.Data.Model;
+using GSF.IO;
+using GSF.Threading;
+
+namespace BulkCalculationState
+{
+    public partial class BulkCalculationState : Form
+    {
+        private const string SourceApp = "openHistorian";
+
+        private string m_sourcePath;
+        private AdoDataConnection m_connection;
+        private TableOperations<CustomActionAdapter> m_actionAdapterTable;
+        private List<CustomActionAdapter> m_actionAdapters;
+        private ShortSynchronizedOperation m_updateTotals;
+
+        public BulkCalculationState() => InitializeComponent();
+
+        private void BulkCalculationState_Load(object sender, EventArgs e)
+        {
+            m_sourcePath = FilePath.GetAbsolutePath("");
+            string configFile = $@"{m_sourcePath}\{SourceApp}.exe.config";
+
+            if (!File.Exists(configFile))
+            {
+                m_sourcePath = $@"C:\Program Files\{SourceApp}";
+                configFile = $@"{m_sourcePath}\{SourceApp}.exe.config";
+            }
+
+            if (!File.Exists(configFile))
+                throw new FileNotFoundException($"Config file for {SourceApp} application \"{configFile}\" was not found.");
+
+            XDocument serviceConfig = XDocument.Load(configFile);
+
+            string connectionString = serviceConfig
+                .Descendants("systemSettings")
+                .SelectMany(systemSettings => systemSettings.Elements("add"))
+                .Where(element => "ConnectionString".Equals((string)element.Attribute("name"), StringComparison.OrdinalIgnoreCase))
+                .Select(element => (string)element.Attribute("value"))
+                .FirstOrDefault();
+
+            string dataProviderString = serviceConfig
+                .Descendants("systemSettings")
+                .SelectMany(systemSettings => systemSettings.Elements("add"))
+                .Where(element => "DataProviderString".Equals((string)element.Attribute("name"), StringComparison.OrdinalIgnoreCase))
+                .Select(element => (string)element.Attribute("value"))
+                .FirstOrDefault();
+
+            m_connection = new AdoDataConnection(connectionString, dataProviderString);
+            m_actionAdapterTable = new TableOperations<CustomActionAdapter>(m_connection);
+            m_actionAdapters = m_actionAdapterTable.QueryRecordsWhere("TypeName = 'DynamicCalculator.DynamicCalculator'").ToList();
+
+            checkedListBoxDevices.DataSource = m_actionAdapters;
+            checkedListBoxDevices.DisplayMember = "AdapterName";
+            checkedListBoxDevices.ValueMember = "Enabled";
+
+            m_updateTotals = new ShortSynchronizedOperation(UpdateTotals);
+
+            SyncCheckedListBox();
+        }
+
+        private void checkedListBoxDevices_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            m_updateTotals?.RunOnceAsync();
+        }
+
+        private void checkBoxSelectAll_CheckedChanged(object sender, EventArgs e)
+        {
+            bool enabled = checkBoxSelectAll.Checked;
+
+            foreach (CustomActionAdapter actionAdapter in m_actionAdapters)
+                actionAdapter.Enabled = enabled;
+
+            SyncCheckedListBox();
+        }
+
+        private void buttonEnableSelected_Click(object sender, EventArgs e)
+        {
+            SyncDataSource();
+
+            foreach (CustomActionAdapter actionAdapter in m_actionAdapters)
+                m_actionAdapterTable.UpdateRecord(actionAdapter);
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                UseShellExecute = false, 
+                RedirectStandardInput = true, 
+                FileName = $@"{m_sourcePath}\{SourceApp}Console.exe"
+            };
+
+            Process process = new Process { StartInfo = startInfo };
+
+            process.Start();
+            process.StandardInput.WriteLine("ReloadConfig");
+            process.StandardInput.WriteLine("Exit");
+
+            process.WaitForExit();
+        }
+
+        private void buttonFind_Click(object sender, EventArgs e)
+        {
+            SyncDataSource();
+
+            string search = textBoxSearch.Text;
+            bool enabled = checkBoxSelect.Checked;
+
+            foreach (CustomActionAdapter actionAdapter in m_actionAdapters)
+            {
+                if (actionAdapter.AdapterName.IndexOf(search, StringComparison.OrdinalIgnoreCase) >= 0)
+                    actionAdapter.Enabled = enabled;
+            }
+
+            SyncCheckedListBox();
+        }
+
+        private void SyncCheckedListBox()
+        {
+            for (int i = 0; i < checkedListBoxDevices.Items.Count; i++)
+            {
+                if (checkedListBoxDevices.Items[i] is CustomActionAdapter actionAdapter)
+                    checkedListBoxDevices.SetItemChecked(i, actionAdapter.Enabled);
+            }
+
+            m_updateTotals?.RunOnceAsync();
+        }
+
+        private void SyncDataSource()
+        {
+            for (int i = 0; i < checkedListBoxDevices.Items.Count; i++)
+            {
+                if (checkedListBoxDevices.Items[i] is CustomActionAdapter actionAdapter)
+                    actionAdapter.Enabled = checkedListBoxDevices.GetItemChecked(i);
+            }
+
+            m_updateTotals?.RunOnceAsync();
+        }
+
+        private void UpdateTotals()
+        {
+            BeginInvoke(new Action(() =>
+            {
+                labelTotal.Text = string.Format(labelTotal.Tag.ToString(), checkedListBoxDevices.Items.Count);
+                labelSelected.Text = string.Format(labelSelected.Tag.ToString(), checkedListBoxDevices.CheckedItems.Count);
+            }));
+        }
+    }
+}
