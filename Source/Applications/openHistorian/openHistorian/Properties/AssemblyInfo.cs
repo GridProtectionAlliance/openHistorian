870daf7785ab1910412db5c0eed3c0744b494bb9
gsfbuildbot <support@gridprotectionalliance.org>
2021-03-21 00:17:24 -0400

    openHistorian: Version change for build v2.8.13.0-master.

--- Source/Applications/openHistorian/openHistorian/Properties/AssemblyInfo.cs
+++ Source/Applications/openHistorian/openHistorian/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
 
-[assembly: AssemblyVersion("2.8.12.0")]
-[assembly: AssemblyFileVersion("2.8.12.0")]
+[assembly: AssemblyVersion("2.8.13.0")]
+[assembly: AssemblyFileVersion("2.8.13.0")]
--- Source/Libraries/GSF.SortedTreeStore/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.SortedTreeStore/Properties/AssemblyInfo.cs
@@ -37,10 +37,10 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.8.12.0")]
+// [assembly: AssemblyVersion("2.8.13.0")]
 
-[assembly: AssemblyVersion("2.8.12.0")]
-[assembly: AssemblyFileVersion("2.8.12.0")]
+[assembly: AssemblyVersion("2.8.13.0")]
+[assembly: AssemblyFileVersion("2.8.13.0")]
 [assembly: InternalsVisibleTo("GSF.SortedTreeStore.Test")]
 [assembly: InternalsVisibleTo("openHistorian.PerformanceTests")]
 [assembly: InternalsVisibleTo("ArchiveIntegrityChecker")]
