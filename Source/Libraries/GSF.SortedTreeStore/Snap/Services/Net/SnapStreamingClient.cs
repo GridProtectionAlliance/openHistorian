c7e93b3a6ba832a9f245ab952732220b2cce02e2
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2021-04-08 16:39:46 -0500

    Fixed security warning in SnapStreamClient Also simple code refresh

--- Source/Libraries/GSF.SortedTreeStore/Snap/Services/Net/SnapStreamingClient.cs
+++ Source/Libraries/GSF.SortedTreeStore/Snap/Services/Net/SnapStreamingClient.cs
@@ -28,10 +28,12 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using GSF.Diagnostics;
 using GSF.Net;
 using GSF.Security;
 using GSF.IO;
 
+// ReSharper disable SwitchStatementHandlesSomeKnownEnumValuesWithDefault
 namespace GSF.Snap.Services.Net
 {
     /// <summary>
@@ -43,12 +45,10 @@ namespace GSF.Snap.Services.Net
         private bool m_disposed;
         private Stream m_rawStream;
         private Stream m_secureStream;
-
         private RemoteBinaryStream m_stream;
         private ClientDatabaseBase m_sortedTreeEngine;
         private string m_historianDatabaseString;
         private SecureStreamClientBase m_credentials;
-
         private Dictionary<string, DatabaseInfo> m_databaseInfos;
 
         /// <summary>
@@ -68,7 +68,6 @@ namespace GSF.Snap.Services.Net
         /// </summary>
         protected SnapStreamingClient()
         {
-
         }
 
         /// <summary>
@@ -79,17 +78,13 @@ namespace GSF.Snap.Services.Net
         /// <param name="useSsl">specifies if a ssl connection is desired.</param>
         protected void Initialize(Stream stream, SecureStreamClientBase credentials, bool useSsl)
         {
-            if (stream is null)
-                throw new ArgumentNullException("stream");
-            if (credentials is null)
-                throw new ArgumentNullException("credentials");
-
-            m_credentials = credentials;
-            m_rawStream = stream;
+            m_credentials = credentials ?? throw new ArgumentNullException(nameof(credentials));
+            m_rawStream = stream ?? throw new ArgumentNullException(nameof(stream));
             m_rawStream.Write(0x2BA517361121L);
             m_rawStream.Write(useSsl); //UseSSL
 
             ServerResponse command = (ServerResponse)m_rawStream.ReadNextByte();
+
             switch (command)
             {
                 case ServerResponse.UnknownProtocol:
@@ -97,7 +92,7 @@ namespace GSF.Snap.Services.Net
                 case ServerResponse.KnownProtocol:
                     break;
                 default:
-                    throw new Exception("Unknown server response: " + command.ToString());
+                    throw new Exception($"Unknown server response: {command}");
             }
 
             useSsl = m_rawStream.ReadBoolean();
@@ -108,17 +103,18 @@ namespace GSF.Snap.Services.Net
             m_stream = new RemoteBinaryStream(m_secureStream);
 
             command = (ServerResponse)m_stream.ReadUInt8();
+            
             switch (command)
             {
                 case ServerResponse.UnhandledException:
                     string exception = m_stream.ReadString();
-                    throw new Exception("Server UnhandledExcetion: \n" + exception);
+                    throw new Exception($"Server UnhandledExcetion: \n{exception}");
                 case ServerResponse.UnknownProtocol:
                     throw new Exception("Client and server cannot agree on a protocol, this is commonly because they are running incompatible versions.");
                 case ServerResponse.ConnectedToRoot:
                     break;
                 default:
-                    throw new Exception("Unknown server response: " + command.ToString());
+                    throw new Exception($"Unknown server response: {command}");
             }
 
             RefreshDatabaseInfo();
@@ -130,24 +126,27 @@ namespace GSF.Snap.Services.Net
             m_stream.Flush();
 
             ServerResponse command = (ServerResponse)m_stream.ReadUInt8();
+            
             switch (command)
             {
                 case ServerResponse.UnhandledException:
                     string exception = m_stream.ReadString();
-                    throw new Exception("Server UnhandledExcetion: \n" + exception);
+                    throw new Exception($"Server UnhandledExcetion: \n{exception}");
                 case ServerResponse.ListOfDatabases:
-                    int cnt = m_stream.ReadInt32();
+                    int count = m_stream.ReadInt32();
                     Dictionary<string, DatabaseInfo> dict = new Dictionary<string, DatabaseInfo>();
-                    while (cnt > 0)
+
+                    while (count > 0)
                     {
-                        cnt--;
+                        count--;
                         DatabaseInfo info = new DatabaseInfo(m_stream);
                         dict.Add(info.DatabaseName.ToUpper(), info);
                     }
+
                     m_databaseInfos = dict;
                     break;
                 default:
-                    throw new Exception("Unknown server response: " + command.ToString());
+                    throw new Exception($"Unknown server response: {command}");
             }
         }
 
@@ -161,8 +160,11 @@ namespace GSF.Snap.Services.Net
             DatabaseInfo info = m_databaseInfos[databaseName.ToUpper()];
             Type type = typeof(SnapStreamingClient);
             MethodInfo method = type.GetMethod("InternalGetDatabase", BindingFlags.NonPublic | BindingFlags.Instance);
+            
+            // ReSharper disable once PossibleNullReferenceException
             MethodInfo reflectionMethod = method.MakeGenericMethod(info.KeyType, info.ValueType);
             ClientDatabaseBase db = (ClientDatabaseBase)reflectionMethod.Invoke(this, new object[] { databaseName });
+            
             return db;
         }
 
@@ -180,29 +182,23 @@ namespace GSF.Snap.Services.Net
         /// </summary>
         /// <param name="databaseName">Name of database instance to access.</param>
         /// <returns><see cref="ClientDatabaseBase{TKey,TValue}"/> for given <paramref name="databaseName"/>.</returns>
-        public override ClientDatabaseBase<TKey, TValue> GetDatabase<TKey, TValue>(string databaseName)
-        {
-            return GetDatabase<TKey, TValue>(databaseName, null);
-        }
+        public override ClientDatabaseBase<TKey, TValue> GetDatabase<TKey, TValue>(string databaseName) => 
+            GetDatabase<TKey, TValue>(databaseName, null);
 
         /// <summary>
         /// Gets basic information for every database connected to the server.
         /// </summary>
         /// <returns></returns>
-        public override List<DatabaseInfo> GetDatabaseInfo()
-        {
-            return m_databaseInfos.Values.ToList();
-        }
+        public override List<DatabaseInfo> GetDatabaseInfo() => 
+            m_databaseInfos.Values.ToList();
 
         /// <summary>
         /// Determines if <see cref="databaseName"/> is contained in the database.
         /// </summary>
         /// <param name="databaseName">Name of database instance to access.</param>
         /// <returns></returns>
-        public override bool Contains(string databaseName)
-        {
-            return m_databaseInfos.ContainsKey(databaseName.ToUpper());
-        }
+        public override bool Contains(string databaseName) => 
+            m_databaseInfos.ContainsKey(databaseName.ToUpper());
 
         /// <summary>
         /// Accesses <see cref="StreamingClientDatabase{TKey,TValue}"/> for given <paramref name="databaseName"/>.
@@ -210,7 +206,7 @@ namespace GSF.Snap.Services.Net
         /// <param name="databaseName">Name of database instance to access.</param>
         /// <param name="encodingMethod"></param>
         /// <returns><see cref="StreamingClientDatabase{TKey,TValue}"/> for given <paramref name="databaseName"/>.</returns>
-        private StreamingClientDatabase<TKey, TValue> GetDatabase<TKey, TValue>(string databaseName, EncodingDefinition encodingMethod = null)
+        private StreamingClientDatabase<TKey, TValue> GetDatabase<TKey, TValue>(string databaseName, EncodingDefinition encodingMethod)
             where TKey : SnapTypeBase<TKey>, new()
             where TValue : SnapTypeBase<TValue>, new()
         {
@@ -218,13 +214,13 @@ namespace GSF.Snap.Services.Net
 
             if (encodingMethod is null)
                 encodingMethod = dbInfo.SupportedStreamingModes.First();
+
             if (m_sortedTreeEngine != null)
-            {
-                throw new Exception("Can only connect to one database at a time. Please disconnect from database" + m_historianDatabaseString);
-            }
+                throw new Exception($"Can only connect to one database at a time. Please disconnect from database{m_historianDatabaseString}");
 
             if (dbInfo.KeyType != typeof(TKey))
                 throw new InvalidCastException("Key types do not match");
+
             if (dbInfo.ValueType != typeof(TValue))
                 throw new InvalidCastException("Value types do not match");
 
@@ -235,13 +231,14 @@ namespace GSF.Snap.Services.Net
             m_stream.Flush();
 
             ServerResponse command = (ServerResponse)m_stream.ReadUInt8();
+
             switch (command)
             {
                 case ServerResponse.UnhandledException:
                     string exception = m_stream.ReadString();
-                    throw new Exception("Server UnhandledExcetion: \n" + exception);
+                    throw new Exception($"Server UnhandledExcetion: \n{exception}");
                 case ServerResponse.DatabaseDoesNotExist:
-                    throw new Exception("Database does not exist on the server: " + databaseName);
+                    throw new Exception($"Database does not exist on the server: {databaseName}");
                 case ServerResponse.DatabaseKeyUnknown:
                     throw new Exception("Database key does not match that passed to this function");
                 case ServerResponse.DatabaseValueUnknown:
@@ -249,7 +246,7 @@ namespace GSF.Snap.Services.Net
                 case ServerResponse.SuccessfullyConnectedToDatabase:
                     break;
                 default:
-                    throw new Exception("Unknown server response: " + command.ToString());
+                    throw new Exception($"Unknown server response: {command}");
             }
 
             StreamingClientDatabase<TKey, TValue> db = new StreamingClientDatabase<TKey, TValue>(m_stream, () => m_sortedTreeEngine = null, dbInfo);
@@ -267,37 +264,38 @@ namespace GSF.Snap.Services.Net
         /// <param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
         protected override void Dispose(bool disposing)
         {
-            if (!m_disposed)
+            if (m_disposed)
+                return;
+
+            try
             {
+                if (!disposing)
+                    return;
+
+                if (m_sortedTreeEngine != null)
+                    m_sortedTreeEngine.Dispose();
+
+                m_sortedTreeEngine = null;
+
                 try
                 {
-                    // This will be done regardless of whether the object is finalized or disposed.
-
-                    if (disposing)
-                    {
-                        if (m_sortedTreeEngine != null)
-                            m_sortedTreeEngine.Dispose();
-                        m_sortedTreeEngine = null;
-                        try
-                        {
-                            m_stream.Write((byte)ServerCommand.Disconnect);
-                            m_stream.Flush();
-                        }
-                        catch (Exception)
-                        {
-
-                        }
-
-                        if (m_rawStream != null)
-                            m_rawStream.Dispose();
-                        m_rawStream = null;
-                    }
+                    m_stream.Write((byte)ServerCommand.Disconnect);
+                    m_stream.Flush();
                 }
-                finally
+                catch (Exception ex)
                 {
-                    m_disposed = true;          // Prevent duplicate dispose.
-                    base.Dispose(disposing);    // Call base class Dispose().
+                    Logger.SwallowException(ex);
                 }
+
+                if (m_rawStream != null)
+                    m_rawStream.Dispose();
+
+                m_rawStream = null;
+            }
+            finally
+            {
+                m_disposed = true;       // Prevent duplicate dispose.
+                base.Dispose(disposing); // Call base class Dispose().
             }
         }
     }
