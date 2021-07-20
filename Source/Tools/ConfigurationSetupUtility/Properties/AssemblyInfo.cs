d77190c3453c3b7f6aa3c334a534de0cee42c9ea
gsfbuildbot <support@gridprotectionalliance.org>
2021-07-20 00:19:34 -0400

    openHistorian: Version change for build v2.8.86.0-master.

--- Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
@@ -53,7 +53,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.8.85.0")]
+// [assembly: AssemblyVersion("2.8.86.0")]
 
-[assembly: AssemblyVersion("2.8.85.0")]
-[assembly: AssemblyFileVersion("2.8.85.0")]
+[assembly: AssemblyVersion("2.8.86.0")]
+[assembly: AssemblyFileVersion("2.8.86.0")]
