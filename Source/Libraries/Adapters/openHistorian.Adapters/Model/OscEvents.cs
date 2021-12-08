126f89549bfe1a4b70f09e927a2131947b2e4b9c
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2021-12-08 16:59:34 -0600

    Added oscillation event model

---
+++ Source/Libraries/Adapters/openHistorian.Adapters/Model/OscEvents.cs
@@ -0,0 +1,37 @@
+﻿// ReSharper disable CheckNamespace
+#pragma warning disable 1591
+
+using GSF.ComponentModel.DataAnnotations;
+using GSF.Data.Model;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace openHistorian.Model
+{
+    public class OscEvents
+    {
+        [PrimaryKey(true)]
+        public int ID { get; set; }
+
+        [Label("Oscillation Source")]
+        [Required]
+        [StringLength(200)]
+        [Searchable]
+        public string Source { get; set; }
+
+        [Label("Start Time")]
+        public DateTime? StartTime { get; set; }
+
+        [Label("Stop Time")]
+        public DateTime? StopTime { get; set; }
+
+        [StringLength(200)]
+        public string Frequency { get; set; }
+
+        [StringLength(200)]
+        public string Magnitude { get; set; }
+
+        [Searchable]
+        public string Notes { get; set; }
+    }
+}
