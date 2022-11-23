06a4a5e1b80c1afe447b8a7c0dac6ca3e29de4c8
gsfbuildbot <support@gridprotectionalliance.org>
2022-11-23 00:12:46 -0500

    Updated GSF dependencies.

--- Source/Data/MySQL/openHistorian.sql
+++ Source/Data/MySQL/openHistorian.sql
@@ -1949,4 +1949,26 @@ SELECT
     CompressionSetting.CompressionMinTime,
     CompressionSetting.CompressionMaxTime,
     CompressionSetting.CompressionLimit
+FROM CompressionSetting CROSS JOIN Node; 
+-- *******************************************************************************************
+-- IMPORTANT NOTE: When making updates to this schema, please increment the version number!
+-- *******************************************************************************************
+CREATE VIEW LocalSchemaVersion AS
+SELECT 1 AS VersionNumber;
+
+CREATE TABLE CompressionSetting(
+    PointID INT NOT NULL,
+    CompressionMinTime BIGINT NOT NULL DEFAULT 0,
+    CompressionMaxTime BIGINT NOT NULL DEFAULT 0,
+    CompressionLimit DOUBLE NOT NULL DEFAULT 0.0,
+ CONSTRAINT PK_CompressionSetting PRIMARY KEY (PointID ASC) 
+);
+
+CREATE VIEW NodeCompressionSetting AS
+SELECT
+    Node.ID AS NodeID,
+    CompressionSetting.PointID,
+    CompressionSetting.CompressionMinTime,
+    CompressionSetting.CompressionMaxTime,
+    CompressionSetting.CompressionLimit
 FROM CompressionSetting CROSS JOIN Node;
