cdcd420a40b70267c820f0cc9a1a0c34d3b976c2
gsfbuildbot <support@gridprotectionalliance.org>
2022-11-25 00:13:55 -0500

    Updated GSF dependencies.

--- Source/Data/SQL Server/openHistorian.sql
+++ Source/Data/SQL Server/openHistorian.sql
@@ -3345,3 +3345,36 @@ SELECT
     CompressionSetting.CompressionLimit
 FROM CompressionSetting CROSS JOIN Node
 GO
+ 
+-- *******************************************************************************************
+-- IMPORTANT NOTE: When making updates to this schema, please increment the version number!
+-- *******************************************************************************************
+CREATE VIEW [dbo].[LocalSchemaVersion] AS
+SELECT 1 AS VersionNumber
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[CompressionSetting](
+    [PointID] [int] NOT NULL,
+    [CompressionMinTime] [bigint] NOT NULL DEFAULT ((0)),
+    [CompressionMaxTime] [bigint] NOT NULL DEFAULT ((0)),
+    [CompressionLimit] [float] NOT NULL DEFAULT ((0.0)),
+ CONSTRAINT [PK_CompressionSetting] PRIMARY KEY CLUSTERED 
+(
+    [PointID] ASC
+)WITH (IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE VIEW NodeCompressionSetting AS
+SELECT
+    Node.ID AS NodeID,
+    CompressionSetting.PointID,
+    CompressionSetting.CompressionMinTime,
+    CompressionSetting.CompressionMaxTime,
+    CompressionSetting.CompressionLimit
+FROM CompressionSetting CROSS JOIN Node
+GO
