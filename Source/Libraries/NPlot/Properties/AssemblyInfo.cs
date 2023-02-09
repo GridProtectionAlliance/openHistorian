363b220410abbc35acc9e8d801123e04f72119de
gsfbuildbot <support@gridprotectionalliance.org>
2023-02-09 01:27:37 -0500

    openHistorian: Version change for build v2.8.249.0-master.

--- Source/Libraries/NPlot/Properties/AssemblyInfo.cs
+++ Source/Libraries/NPlot/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.8.248.0")]
+// [assembly: AssemblyVersion("2.8.249.0")]
 
-[assembly: AssemblyVersion("2.8.248.0")]
-[assembly: AssemblyFileVersion("2.8.248.0")]
+[assembly: AssemblyVersion("2.8.249.0")]
+[assembly: AssemblyFileVersion("2.8.249.0")]
