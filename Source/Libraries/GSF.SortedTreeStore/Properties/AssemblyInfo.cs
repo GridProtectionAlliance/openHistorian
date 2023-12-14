68c8363e59fd5c151add53b0c364947934f6889f
gsfbuildbot <support@gridprotectionalliance.org>
2023-12-14 00:23:42 -0500

    openHistorian: Version change for build v2.8.353.0-master.

--- Source/Libraries/GSF.SortedTreeStore/Properties/AssemblyInfo.cs
+++ Source/Libraries/GSF.SortedTreeStore/Properties/AssemblyInfo.cs
@@ -37,10 +37,10 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.8.352.0")]
+// [assembly: AssemblyVersion("2.8.353.0")]
 
-[assembly: AssemblyVersion("2.8.352.0")]
-[assembly: AssemblyFileVersion("2.8.352.0")]
+[assembly: AssemblyVersion("2.8.353.0")]
+[assembly: AssemblyFileVersion("2.8.353.0")]
 [assembly: InternalsVisibleTo("GSF.SortedTreeStore.Test")]
 [assembly: InternalsVisibleTo("openHistorian.Adapters")]
 [assembly: InternalsVisibleTo("openHistorian.PerformanceTests")]
--- Source/Libraries/NPlot/Properties/AssemblyInfo.cs
+++ Source/Libraries/NPlot/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.8.352.0")]
+// [assembly: AssemblyVersion("2.8.353.0")]
 
-[assembly: AssemblyVersion("2.8.352.0")]
-[assembly: AssemblyFileVersion("2.8.352.0")]
+[assembly: AssemblyVersion("2.8.353.0")]
+[assembly: AssemblyFileVersion("2.8.353.0")]
