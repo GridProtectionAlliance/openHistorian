d5243af74c3c13e85a6624b0b29e2a864461914b
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2024-03-08 21:53:18 -0600

    Improved array of array retrun value formatting

--- Source/Applications/openHistorian/openHistorian/ServiceHost.cs
+++ Source/Applications/openHistorian/openHistorian/ServiceHost.cs
@@ -888,7 +888,7 @@ namespace openHistorian
                 if (!signaled)
                     return (HttpStatusCode.RequestTimeout, "Command return value request timed out.");
 
-                response = state.ReturnValue;
+                response = state.ReturnValue; 
             }
             else
             {
@@ -902,16 +902,20 @@ namespace openHistorian
         // Intercept responses to client requests to capture return value
         protected override void SendResponseWithAttachment(ClientRequestInfo requestInfo, bool success, object attachment, string status, params object[] args)
         {
+            static string ToStringRepresentation(object value, string separator)
+            {
+                return value switch
+                {
+                    null => "null",
+                    Array array => string.Join(separator, array.OfType<object>().Select(val => ToStringRepresentation(val, " "))),
+                    _ => value.ToString()
+                };
+            }
+
             // Look for requests that were generated locally
             if (m_returnValueStates.TryGetValue(requestInfo, out ReturnValueState state) && state is not null)
             {
-                if (attachment is not null)
-                {
-                    state.ReturnValue = attachment is Array array ?
-                        string.Join(",", array.OfType<object>().Select(val => val.ToString())) :
-                        attachment.ToString();
-                }
-
+                state.ReturnValue = ToStringRepresentation(attachment, ",");
                 state.WaitHandle?.Set();
             }
 
