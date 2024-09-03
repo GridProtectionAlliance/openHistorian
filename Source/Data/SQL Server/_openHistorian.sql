b04819a9c0c2b17e07409472c51be16b0038fe48
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2024-09-03 13:51:28 -0500

    Updated local sub-schema version for SQL Server

--- Source/Data/SQL Server/_openHistorian.sql
+++ Source/Data/SQL Server/_openHistorian.sql
@@ -2,7 +2,7 @@
 -- IMPORTANT NOTE: When making updates to this schema, please increment the version number!
 -- *******************************************************************************************
 CREATE VIEW [dbo].[LocalSchemaVersion] AS
-SELECT 1 AS VersionNumber
+SELECT 2 AS VersionNumber
 GO
 
 SET ANSI_NULLS ON
