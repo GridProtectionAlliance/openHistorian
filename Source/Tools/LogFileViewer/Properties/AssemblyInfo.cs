634019d6f5bf531d536c36400a6c4a624fd86b74
gsfbuildbot <support@gridprotectionalliance.org>
2024-11-02 00:24:11 -0400

    openHistorian: Version change for build v2.8.424.0-master.

--- Source/Tools/LogFileViewer/Properties/AssemblyInfo.cs
+++ Source/Tools/LogFileViewer/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.8.423.0")]
-[assembly: AssemblyVersion("2.8.423.0")]
-[assembly: AssemblyFileVersion("2.8.423.0")]
+// [assembly: AssemblyVersion("2.8.424.0")]
+[assembly: AssemblyVersion("2.8.424.0")]
+[assembly: AssemblyFileVersion("2.8.424.0")]
