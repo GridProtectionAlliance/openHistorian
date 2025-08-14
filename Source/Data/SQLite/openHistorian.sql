1b69730f3623a6379b4892805be721bd253fceeb
gsfbuildbot <support@gridprotectionalliance.org>
2025-08-14 00:25:17 -0400

    Updated GSF dependencies.

--- Source/Data/SQLite/openHistorian.sql
+++ Source/Data/SQLite/openHistorian.sql
@@ -1788,4 +1788,35 @@ CREATE TABLE EventMarker(
     StopTime DATETIME NULL,
     Notes TEXT NULL,
     CONSTRAINT FK_EventMarker_EventMarker FOREIGN KEY(ParentID) REFERENCES EventMarker (ID)
+); 
+-- *******************************************************************************************
+-- IMPORTANT NOTE: When making updates to this schema, please increment the version number!
+-- *******************************************************************************************
+CREATE VIEW LocalSchemaVersion AS
+SELECT 2 AS VersionNumber;
+
+CREATE TABLE CompressionSetting(
+    PointID INTEGER PRIMARY KEY NOT NULL,
+    CompressionMinTime INTEGER NOT NULL DEFAULT 0,
+    CompressionMaxTime INTEGER NOT NULL DEFAULT 0,
+    CompressionLimit REAL NOT NULL DEFAULT 0.0
+ );
+
+CREATE VIEW NodeCompressionSetting AS
+SELECT
+    Node.ID AS NodeID,
+    CompressionSetting.PointID,
+    CompressionSetting.CompressionMinTime,
+    CompressionSetting.CompressionMaxTime,
+    CompressionSetting.CompressionLimit
+FROM CompressionSetting CROSS JOIN Node;
+
+CREATE TABLE EventMarker(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    ParentID INTEGER NULL,
+    Source VARCHAR(200) NULL,
+    StartTime DATETIME NULL,
+    StopTime DATETIME NULL,
+    Notes TEXT NULL,
+    CONSTRAINT FK_EventMarker_EventMarker FOREIGN KEY(ParentID) REFERENCES EventMarker (ID)
 );
