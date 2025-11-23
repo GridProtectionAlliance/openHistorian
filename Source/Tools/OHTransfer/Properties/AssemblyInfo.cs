9e133187d97e8c0e6119931192a6272541c6f8bf
gsfbuildbot <support@gridprotectionalliance.org>
2025-11-23 02:07:35 -0500

    openHistorian: Version change for build v2.8.505.0-master.

--- Source/Tools/OHTransfer/Properties/AssemblyInfo.cs
+++ Source/Tools/OHTransfer/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.8.504.0")]
-[assembly: AssemblyFileVersion("2.8.504.0")]
+[assembly: AssemblyVersion("2.8.505.0")]
+[assembly: AssemblyFileVersion("2.8.505.0")]
